bf4bec5ca08394f2e8e86bc0ea1b42c3cd966f6a
mobitopp <mobitopp@ifv.kit.edu>
2018-05-04 13:10:45 +0200

    Add Leopoldshafen as example.

---
+++ example/src/main/java/edu/kit/ifv/mobitopp/simulation/SimulationExample.java
@@ -0,0 +1,90 @@
+package edu.kit.ifv.mobitopp.simulation;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import edu.kit.ifv.mobitopp.simulation.activityschedule.randomizer.DefaultActivityDurationRandomizer;
+import edu.kit.ifv.mobitopp.simulation.destinationChoice.CarRangeReachableZonesFilter;
+import edu.kit.ifv.mobitopp.simulation.destinationChoice.DestinationChoiceForFlexibleActivity;
+import edu.kit.ifv.mobitopp.simulation.destinationChoice.DestinationChoiceModel;
+import edu.kit.ifv.mobitopp.simulation.destinationChoice.DestinationChoiceWithFixedLocations;
+import edu.kit.ifv.mobitopp.simulation.destinationChoice.SimpleRepeatedDestinationChoice;
+import edu.kit.ifv.mobitopp.simulation.modeChoice.ModeAvailabilityModel;
+import edu.kit.ifv.mobitopp.simulation.modeChoice.ModeAvailabilityModelAddingCarsharing;
+import edu.kit.ifv.mobitopp.simulation.modeChoice.ModeChoiceModel;
+import edu.kit.ifv.mobitopp.simulation.modeChoice.stuttgart.ModeChoiceStuttgart;
+import edu.kit.ifv.mobitopp.simulation.modeChoice.stuttgart.ModeSelectorParameterFirstTrip;
+import edu.kit.ifv.mobitopp.simulation.modeChoice.stuttgart.ModeSelectorParameterOtherTrip;
+import edu.kit.ifv.mobitopp.simulation.person.PersonStateSimple;
+
+public class SimulationExample extends Simulation {
+
+	public SimulationExample(SimulationContext context) {
+		super(context);
+	}
+
+	@Override
+	protected DemandSimulator simulator() {
+		ModeAvailabilityModel modeAvailabilityModel = new ModeAvailabilityModelAddingCarsharing(
+				impedance());
+		DestinationChoiceModel targetSelector = destinationChoiceModel(impedance(), modeAvailabilityModel);
+		ModeChoiceModel modeSelector = modeChoiceModel(impedance(), modeAvailabilityModel);
+		ZoneBasedRouteChoice routeChoice = new NoRouteChoice();
+		ReschedulingStrategy rescheduling = new ReschedulingSkipTillHome(context().simulationDays());
+		System.out.println("Initializing simulator...");
+		return new DemandSimulatorPassenger(targetSelector, modeSelector, routeChoice,
+				new DefaultActivityDurationRandomizer(context().seed()), rescheduling,
+				PersonStateSimple.UNINITIALIZED, context());
+	}
+
+	private ModeChoiceModel modeChoiceModel(
+			ImpedanceIfc impedance, ModeAvailabilityModel modeAvailabilityModel) {
+		ModeChoiceModel modeSelectorFirst = new ModeChoiceStuttgart(impedance,
+				new ModeSelectorParameterFirstTrip());
+		ModeChoiceModel modeSelectorOther = new ModeChoiceStuttgart(impedance,
+				new ModeSelectorParameterOtherTrip());
+		return new ModeSelectorFirstOther(modeAvailabilityModel, modeSelectorFirst, modeSelectorOther);
+	}
+
+	private DestinationChoiceModel destinationChoiceModel(
+			ImpedanceIfc impedance, ModeAvailabilityModel modeAvailabilityModel) {
+		Map<String, String> destinationChoiceFiles = context().configuration().getDestinationChoice();
+
+		return new DestinationChoiceWithFixedLocations(
+				zoneRepository().zones(),
+				new SimpleRepeatedDestinationChoice(
+						zoneRepository().zones(),
+						new DestinationChoiceForFlexibleActivity(
+								modeAvailabilityModel, 
+								new CarRangeReachableZonesFilter(impedance),
+								new AttractivityCalculatorCostNextPole(zoneRepository().zones(),
+										impedance, destinationChoiceFiles.get("cost"), 0.5f)),
+						destinationChoiceFiles.get("repetition")));
+	}
+
+	public static void main(String... args) throws IOException {
+		if (1 > args.length) {
+			System.out.println("Usage: ... <configuration file>");
+			System.exit(-1);
+		}
+
+		File configurationFile = new File(args[0]);
+		LocalDateTime start = LocalDateTime.now();
+		startSimulation(configurationFile);
+		LocalDateTime end = LocalDateTime.now();
+		Duration runtime = Duration.between(start, end);
+		System.out.println("Simulation took " + runtime);
+	}
+
+	private static void startSimulation(File configurationFile) throws IOException {
+		SimulationContext context = new ContextBuilder().buildFrom(configurationFile);
+		startSimulation(context);
+	}
+		
+	public static void startSimulation(SimulationContext context) {
+		new SimulationExample(context).simulate();
+	}
+}
