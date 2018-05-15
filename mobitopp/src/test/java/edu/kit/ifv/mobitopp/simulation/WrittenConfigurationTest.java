8725ab9122322c7d2d3f5bd439e296f05b947b6d
Lars Briem <lars.briem@kit.edu>
2018-05-15 15:54:20 +0200

    Fix copy constructor.

---
+++ mobitopp/src/test/java/edu/kit/ifv/mobitopp/simulation/WrittenConfigurationTest.java
@@ -0,0 +1,27 @@
+package edu.kit.ifv.mobitopp.simulation;
+
+import static edu.kit.ifv.mobitopp.util.TestUtil.assertValue;
+
+import org.junit.Test;
+
+public class WrittenConfigurationTest {
+
+	@Test
+	public void copies() {
+		WrittenConfiguration original = new WrittenConfiguration();
+		
+		WrittenConfiguration copied = new WrittenConfiguration(original);
+		
+		assertValue(WrittenConfiguration::getFractionOfPopulation, copied, original);
+		assertValue(WrittenConfiguration::getSeed, copied, original);
+		assertValue(WrittenConfiguration::getDays, copied, original);
+		assertValue(WrittenConfiguration::getNumberOfZones, copied, original);
+		assertValue(WrittenConfiguration::getDataSource, copied, original);
+		assertValue(WrittenConfiguration::getPublicTransport, copied, original);
+		assertValue(WrittenConfiguration::getResultFolder, copied, original);
+		assertValue(WrittenConfiguration::getVisumFile, copied, original);
+		assertValue(WrittenConfiguration::getDestinationChoice, copied, original);
+		assertValue(WrittenConfiguration::getTimeStepLength, copied, original);
+		assertValue(WrittenConfiguration::getExperimental, copied, original);
+	}
+}
