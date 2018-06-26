817eb367684b311bc30936e27b32f8322c8e9066
Lars Briem <lars.briem@kit.edu>
2018-06-26 11:35:26 +0200

    #189 Let vehicles interact with each other
    Refactor deactivated behaviour.

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/simulation/VehicleBehaviour.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/simulation/VehicleBehaviour.java
@@ -1,67 +1,12 @@
 package edu.kit.ifv.mobitopp.simulation;
 
-import edu.kit.ifv.mobitopp.publictransport.model.Stop;
 import edu.kit.ifv.mobitopp.simulation.events.EventQueue;
 import edu.kit.ifv.mobitopp.simulation.person.PublicTransportBehaviour;
-import edu.kit.ifv.mobitopp.simulation.person.PublicTransportLeg;
-import edu.kit.ifv.mobitopp.simulation.person.PublicTransportTrip;
-import edu.kit.ifv.mobitopp.simulation.person.SimulationPerson;
 import edu.kit.ifv.mobitopp.time.Time;
 
 public interface VehicleBehaviour extends PublicTransportBehaviour {
 
-	public static final VehicleBehaviour empty = new VehicleBehaviour() {
-
-		@Override
-		public void letVehiclesArriveAt(Time currentDate, EventQueue queue) {
-		}
-
-		@Override
-		public void letVehiclesDepartAt(Time currentDate) {
-		}
-
-		@Override
-		public boolean hasVehicleDeparted(PublicTransportLeg leg) {
-			return false;
-		}
-
-		@Override
-		public boolean isVehicleAvailable(PublicTransportLeg leg) {
-			return false;
-		}
-		
-		@Override
-		public boolean hasPlaceInVehicle(PublicTransportLeg leg) {
-			return false;
-		}
-
-		@Override
-		public void board(SimulationPerson person, Time time, PublicTransportLeg part, TripIfc trip) {
-		}
-
-		@Override
-		public void getOff(SimulationPerson person, Time time, PublicTransportLeg part, TripIfc trip) {
-		}
-
-		@Override
-		public TripIfc searchNewTrip(
-				SimulationPerson person, Time time, PublicTransportTrip trip) {
-			return trip;
-		}
-
-		@Override
-		public void wait(SimulationPerson person, Time time, PublicTransportLeg part, TripIfc trip) {
-		}
-
-		@Override
-		public void enterWaitingArea(SimulationPerson simulationPerson, Stop stop) {
-		}
-
-		@Override
-		public void leaveWaitingArea(SimulationPerson person, Stop stop) {
-		}
-
-	};
+	static final VehicleBehaviour noBehaviour = new NoVehicleBehaviour();
 
 	void letVehiclesArriveAt(Time currentDate, EventQueue queue);
 
