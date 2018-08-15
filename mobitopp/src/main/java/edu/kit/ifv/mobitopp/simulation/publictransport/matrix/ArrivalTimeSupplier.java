de39cbf166152c2d242fedc7785e9c0eb63db9b9
Lars Briem <lars.briem@kit.edu>
2018-08-15 16:15:15 +0200

    #202 Serialise zone information for simulation
    Move ZoneBasedPublicTransport into separate package.

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/simulation/publictransport/matrix/ArrivalTimeSupplier.java
+++
@@ -1,9 +0,0 @@
-package edu.kit.ifv.mobitopp.simulation.publictransport.matrix;
-
-import edu.kit.ifv.mobitopp.time.Time;
-
-public interface ArrivalTimeSupplier {
-
-	Time startingAt(Time departure);
-
-}
