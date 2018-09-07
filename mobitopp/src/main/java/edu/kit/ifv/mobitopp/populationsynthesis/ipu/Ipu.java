5ba2469cdfbeda7a78be3a25ac8986310d7292ed
Lars Briem <lars.briem@kit.edu>
2018-09-07 11:04:23 +0200

    #182 Implement IPU based population synthesis
    Refactor Ipu.

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/ipu/Ipu.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/ipu/Ipu.java
@@ -8,6 +8,9 @@ public class Ipu {
 	private final Iteration iteration;
 	private final int maxIterations;
 	private final double maxGoodness;
+	private List<Household> bestHouseholds;
+	private double bestGoodness;
+	private List<Household> currentHouseholds;
 
 	public Ipu(Iteration iteration, int maxIterations, double maxGoodness) {
 		super();
@@ -17,23 +20,43 @@ public class Ipu {
 	}
 
 	public List<Household> adjustWeightsOf(List<Household> households) {
-		List<Household> updatedHouseholds = new ArrayList<>(households);
-		List<Household> lastIterationHouseholds = updatedHouseholds;
-		List<Household> bestHouseholds = updatedHouseholds;
-		double bestGoodness = iteration.calculateGoodnessOfFitFor(households);
+		initialise(households);
+		List<Household> lastIterationHouseholds = currentHouseholds;
 		for (int current = 0; current < maxIterations; current++) {
-			updatedHouseholds = iteration.adjustWeightsOf(lastIterationHouseholds);
-			double goodnessOfFit = iteration.calculateGoodnessOfFitFor(updatedHouseholds);
-			if (Math.abs(goodnessOfFit - bestGoodness) <= maxGoodness) {
-				return updatedHouseholds;
+			updateHouseholds(lastIterationHouseholds);
+			double goodnessOfFit = calculateGoodness();
+			if (converged(goodnessOfFit)) {
+				return currentHouseholds;
 			}
-			lastIterationHouseholds = updatedHouseholds;
-			if (bestGoodness > goodnessOfFit) {
-				bestGoodness = goodnessOfFit;
-				bestHouseholds = updatedHouseholds;
-			}
+			lastIterationHouseholds = currentHouseholds;
+			updateBest(goodnessOfFit);
 		}
 		return bestHouseholds;
+	}
+
+	private void initialise(List<Household> households) {
+		currentHouseholds = new ArrayList<>(households);
+		bestHouseholds = currentHouseholds;
+		bestGoodness = calculateGoodness();
+	}
+
+	private void updateHouseholds(List<Household> lastIterationHouseholds) {
+		currentHouseholds = iteration.adjustWeightsOf(lastIterationHouseholds);
+	}
+
+	private double calculateGoodness() {
+		return iteration.calculateGoodnessOfFitFor(currentHouseholds);
+	}
+
+	private boolean converged(double goodnessOfFit) {
+		return Math.abs(goodnessOfFit - bestGoodness) <= maxGoodness;
+	}
+
+	private void updateBest(double goodnessOfFit) {
+		if (bestGoodness > goodnessOfFit) {
+			bestGoodness = goodnessOfFit;
+			bestHouseholds = currentHouseholds;
+		}
 	}
 
 }
