4982a39f014837f8db05425971204c482d3c7f03
Lars Briem <lars.briem@kit.edu>
2018-09-25 16:51:41 +0200

    #182 Implement IPU based population synthesis
    Simplify filtering of all edges.

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/householdlocation/EdgeFilter.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/householdlocation/EdgeFilter.java
@@ -1,12 +1,13 @@
 package edu.kit.ifv.mobitopp.populationsynthesis.householdlocation;
 
 import java.util.Collection;
-import java.util.List;
 
 import edu.kit.ifv.mobitopp.network.Edge;
 
 public interface EdgeFilter {
 
-	List<Edge> filter(Collection<Edge> edges);
+	EdgeFilter allEdges = edges -> edges;
+
+	Collection<Edge> filter(Collection<Edge> edges);
 
 }
