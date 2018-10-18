37abd2b989bf5a26438748e5b6bbcb5a2ff0a1db
Lars Briem <lars.briem@kit.edu>
2018-10-18 11:08:15 +0200

    #182 Implement IPU based population synthesis
    Extract old code to print distributions.

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/PrintDistribution.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/PrintDistribution.java
@@ -6,17 +6,20 @@ import java.util.function.Function;
 import edu.kit.ifv.mobitopp.data.DemandZone;
 import edu.kit.ifv.mobitopp.data.DemandZoneRepository;
 import edu.kit.ifv.mobitopp.data.demand.Demography;
-import edu.kit.ifv.mobitopp.data.demand.EmploymentDistribution;
-import edu.kit.ifv.mobitopp.data.demand.EmploymentDistributionItem;
-import edu.kit.ifv.mobitopp.result.Logger;
 
 public class PrintDistribution {
 
 	private final DemandZoneRepository zoneRepository;
+	private final PrintEmployment employment;
+	private final PrintMaleAge maleAge;
+	private final PrintFemaleAge femaleAge;
 
 	public PrintDistribution(DemandZoneRepository zoneRepository) {
 		super();
 		this.zoneRepository = zoneRepository;
+		employment = new PrintEmployment(zoneRepository, System.out::println);
+		maleAge = new PrintMaleAge(zoneRepository, System.out::println);
+		femaleAge = new PrintFemaleAge(zoneRepository, System.out::println);
 	}
 
 	protected List<DemandZone> getZones() {
@@ -32,34 +35,9 @@ public class PrintDistribution {
 	}
 
 	private void print(Function<DemandZone, Demography> toDemography, String type) {
-		EmploymentDistribution total = new EmploymentDistribution();
-		for (DemandZone zone : getZones()) {
-			EmploymentDistribution distribution = toDemography.apply(zone).employment();
-			increment(total, distribution);
-		}
-		logger().println("\n" + type + " distribution:");
-		print(total);
+		employment.print(toDemography, type + " employment");
+		femaleAge.print(toDemography, type + " female age");
+		maleAge.print(toDemography, type + " male age");
 	}
 
-	void increment(EmploymentDistribution total, EmploymentDistribution distribution) {
-		for (EmploymentDistributionItem item : distribution.getItems()) {
-			for (int i = 0; i < item.amount(); i++) {
-				total.getItem(item.type()).increment();
-			}
-		}
-	}
-
-	void print(EmploymentDistribution total) {
-		int grand_total = 0;
-		for (EmploymentDistributionItem item : total.getItems()) {
-			logger().println(item.type().getTypeAsString() + ": " + item.amount());
-			grand_total += item.amount();
-		}
-		logger().println("TOTAL\t" + grand_total);
-		logger().println("\n\n");
-	}
-
-	Logger logger() {
-		return System.out::println;
-	}
 }
---
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/PrintFemaleAge.java
@@ -0,0 +1,64 @@
+package edu.kit.ifv.mobitopp.populationsynthesis;
+
+import java.util.List;
+import java.util.function.Function;
+
+import edu.kit.ifv.mobitopp.data.DemandZone;
+import edu.kit.ifv.mobitopp.data.DemandZoneRepository;
+import edu.kit.ifv.mobitopp.data.demand.AgeDistributionItem;
+import edu.kit.ifv.mobitopp.data.demand.Demography;
+import edu.kit.ifv.mobitopp.data.demand.FemaleAgeDistribution;
+import edu.kit.ifv.mobitopp.result.Logger;
+
+public class PrintFemaleAge {
+
+	private final DemandZoneRepository zoneRepository;
+	private final Logger logger;
+
+	public PrintFemaleAge(DemandZoneRepository zoneRepository, Logger logger) {
+		super();
+		this.zoneRepository = zoneRepository;
+		this.logger = logger;
+	}
+
+	public void print(Function<DemandZone, Demography> toDemography, String type) {
+		FemaleAgeDistribution total = new FemaleAgeDistribution();
+		for (DemandZone zone : getZones()) {
+			FemaleAgeDistribution distribution = toDemography.apply(zone).femaleAge();
+			increment(total, distribution);
+		}
+		logger().println("\n" + type + " distribution:");
+		print(total);
+	}
+
+	private List<DemandZone> getZones() {
+		return zoneRepository.getZones();
+	}
+
+	void increment(FemaleAgeDistribution total, FemaleAgeDistribution distribution) {
+		for (AgeDistributionItem item : distribution.getItems()) {
+			if (total.hasItem(item.lowerBound())) {
+				for (int i = 0; i < item.amount(); i++) {
+					total.getItem(item.lowerBound()).increment();
+				}
+			} else {
+				total.addItem(new AgeDistributionItem(item));
+			}
+		}
+	}
+
+	void print(FemaleAgeDistribution total) {
+		int grand_total = 0;
+		for (AgeDistributionItem item : total.getItems()) {
+			logger().println(item.lowerBound() + "-" + item.upperBound() + ": " + item.amount());
+			grand_total += item.amount();
+		}
+		logger().println("TOTAL\t" + grand_total);
+		logger().println("\n\n");
+	}
+
+	Logger logger() {
+		return logger;
+	}
+
+}
