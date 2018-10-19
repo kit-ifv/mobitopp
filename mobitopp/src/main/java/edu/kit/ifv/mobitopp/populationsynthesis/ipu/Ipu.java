77fec837aa56dea7f4f9c755a7e17fd6b80f7f14
Lars Briem <lars.briem@kit.edu>
2018-10-19 10:11:48 +0200

    #182 Implement IPU based population synthesis
    Log best results of ipu.

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/ipu/Ipu.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/ipu/Ipu.java
@@ -3,35 +3,51 @@ package edu.kit.ifv.mobitopp.populationsynthesis.ipu;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.kit.ifv.mobitopp.result.Logger;
+
 public class Ipu {
 
 	private final Iteration iteration;
 	private final int maxIterations;
 	private final double maxGoodness;
+	private final Logger logger;
 	private List<WeightedHousehold> bestHouseholds;
 	private double bestGoodness;
 	private List<WeightedHousehold> currentHouseholds;
+	private int lastIteration;
+	private int bestIteration;
 
-	public Ipu(Iteration iteration, int maxIterations, double maxGoodness) {
+	public Ipu(Iteration iteration, int maxIterations, double maxGoodness, Logger logger) {
 		super();
 		this.iteration = iteration;
 		this.maxIterations = maxIterations;
 		this.maxGoodness = maxGoodness;
+		this.logger = logger;
 	}
 
 	public List<WeightedHousehold> adjustWeightsOf(List<WeightedHousehold> households) {
 		initialise(households);
 		List<WeightedHousehold> lastIterationHouseholds = currentHouseholds;
-		for (int current = 0; current < maxIterations; current++) {
+		for (lastIteration = 0; lastIteration < maxIterations; lastIteration++) {
 			updateHouseholds(lastIterationHouseholds);
 			double goodnessOfFit = calculateGoodness();
 			if (converged(goodnessOfFit)) {
-				return currentHouseholds;
+				updateBest(goodnessOfFit);
+				log();
+				return bestHouseholds;
 			}
 			lastIterationHouseholds = currentHouseholds;
 			updateBest(goodnessOfFit);
 		}
+		log();
 		return bestHouseholds;
+	}
+
+	private void log() {
+		logger
+				.println(String
+						.format("iterations: %s best iteration: %s goodness of fit: %s", lastIteration,
+								bestIteration, bestGoodness));
 	}
 
 	private void initialise(List<WeightedHousehold> households) {
