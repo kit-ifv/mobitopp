1b3fb10bab86d151d656ef2b8614ec297d8f1954
Lars Briem <lars.briem@kit.edu>
2019-01-08 14:41:35 +0100

    Remove printing of nominal demography before population synthesis.

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/PopulationSynthesis.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/PopulationSynthesis.java
@@ -18,143 +18,141 @@ import edu.kit.ifv.mobitopp.simulation.opportunities.OpportunityDataForZone;
 
 public abstract class PopulationSynthesis {
 
-	private final SynthesisContext context;
-	private final DemandCategories categories;
+  private final SynthesisContext context;
+  private final DemandCategories categories;
 
-	public PopulationSynthesis(SynthesisContext context) {
-		super();
-		this.context = context;
-		categories = new DemandCategories();
-	}
+  public PopulationSynthesis(SynthesisContext context) {
+    super();
+    this.context = context;
+    categories = new DemandCategories();
+  }
 
-	SynthesisContext context() {
-		return context;
-	}
+  SynthesisContext context() {
+    return context;
+  }
 
-	protected DataRepositoryForPopulationSynthesis dataRepository() {
-		return context.dataRepository();
-	}
+  protected DataRepositoryForPopulationSynthesis dataRepository() {
+    return context.dataRepository();
+  }
 
-	protected Results results() {
-		return context.resultWriter();
-	}
+  protected Results results() {
+    return context.resultWriter();
+  }
 
-	protected DemandZoneRepository demandZoneRepository() {
-		return context.zoneRepository();
-	}
+  protected DemandZoneRepository demandZoneRepository() {
+    return context.zoneRepository();
+  }
 
-	protected long seed() {
-		return context.seed();
-	}
+  protected long seed() {
+    return context.seed();
+  }
 
-	protected ImpedanceIfc impedance() {
-		return context.impedance();
-	}
+  protected ImpedanceIfc impedance() {
+    return context.impedance();
+  }
 
-	protected List<DemandZone> getZones() {
-		return demandZoneRepository().getZones();
-	}
+  protected List<DemandZone> getZones() {
+    return demandZoneRepository().getZones();
+  }
 
-	public void createPopulation() {
-		createLocations();
-		Map<ActivityType, FixedDistributionMatrix> fdMatrices = fixedDistributionMatrices();
-		assertFixedDistributionMatrices(fdMatrices);
-		printBeforeCreation();
-		doCreatePopulation(fdMatrices);
-		doPrintAfterCreation();
-		finishExecution();
-	}
+  public void createPopulation() {
+    createLocations();
+    Map<ActivityType, FixedDistributionMatrix> fdMatrices = fixedDistributionMatrices();
+    assertFixedDistributionMatrices(fdMatrices);
+    printBeforeCreation();
+    doCreatePopulation(fdMatrices);
+    doPrintAfterCreation();
+    finishExecution();
+  }
 
-	private void createLocations() {
-		System.out.println("creating destinations...");
-		OpportunityLocationSelector locationSelector = createOpportunityLocationSelector();
-		createLocations(locationSelector);
-		System.out.println("creating DONE.");
-	}
-	
-	protected abstract OpportunityLocationSelector createOpportunityLocationSelector();
+  private void createLocations() {
+    System.out.println("creating destinations...");
+    OpportunityLocationSelector locationSelector = createOpportunityLocationSelector();
+    createLocations(locationSelector);
+    System.out.println("creating DONE.");
+  }
 
-	private void createLocations(OpportunityLocationSelector opportunityLocationSelector) {
-		List<Zone> zones = demandZoneRepository().zoneRepository().getZones();
-		for (Zone zone : zones) {
-			System.out.println(
-					"zone " + zone.getId() + " is ready? " + zone.opportunities().locationsAvailable());
-			createLocationsForZone(opportunityLocationSelector, zone);
-		}
-	}
+  protected abstract OpportunityLocationSelector createOpportunityLocationSelector();
 
-	private void createLocationsForZone(
-			OpportunityLocationSelector opportunityLocationSelector, Zone zone) {
-		OpportunityDataForZone opportunities = zone.opportunities();
-		opportunities.createLocations(opportunityLocationSelector);
+  private void createLocations(OpportunityLocationSelector opportunityLocationSelector) {
+    List<Zone> zones = demandZoneRepository().zoneRepository().getZones();
+    for (Zone zone : zones) {
+      System.out
+          .println(
+              "zone " + zone.getId() + " is ready? " + zone.opportunities().locationsAvailable());
+      createLocationsForZone(opportunityLocationSelector, zone);
+    }
+  }
 
-		results().write(categories.demanddataOpportunities, opportunities.forLogging());
-	}
+  private void createLocationsForZone(
+      OpportunityLocationSelector opportunityLocationSelector, Zone zone) {
+    OpportunityDataForZone opportunities = zone.opportunities();
+    opportunities.createLocations(opportunityLocationSelector);
 
-	private void printBeforeCreation() {
-		PrintDistribution printer = new PrintDistribution(context.zoneRepository());
-		printer.printNominalDistributions();
-	}
+    results().write(categories.demanddataOpportunities, opportunities.forLogging());
+  }
+  
+  protected void printBeforeCreation() {
+  }
 
-	private void doPrintAfterCreation() {
-		PrintDistribution printer = new PrintDistribution(context.zoneRepository());
-		printer.printNominalDistributions();
-		printer.printActualDistributions();
-		printAfterCreation();
-	}
+  private void doPrintAfterCreation() {
+    PrintDistribution printer = new PrintDistribution(context.zoneRepository());
+    printer.printNominalDistributions();
+    printer.printActualDistributions();
+    printAfterCreation();
+  }
 
-	protected void printAfterCreation() {
-		
-	}
+  protected void printAfterCreation() {
+  }
 
-	void doCreatePopulation(Map<ActivityType, FixedDistributionMatrix> fdMatrices) {
-		DemandDataForZoneCalculatorIfc calculator = createCalculator(fdMatrices);
-		printZoneInformation();
-		createDemandForZonesWith(calculator);
-	}
+  void doCreatePopulation(Map<ActivityType, FixedDistributionMatrix> fdMatrices) {
+    DemandDataForZoneCalculatorIfc calculator = createCalculator(fdMatrices);
+    printZoneInformation();
+    createDemandForZonesWith(calculator);
+  }
 
-	private void createDemandForZonesWith(DemandDataForZoneCalculatorIfc calculator) {
-		for (DemandZone zone : demandZoneRepository().getZones()) {
-			System.out.println("PopulationSynthesis: Zone " + zone.getId());
-			calculator.calculateDemandData(zone, impedance());
-		}
-	}
+  private void createDemandForZonesWith(DemandDataForZoneCalculatorIfc calculator) {
+    for (DemandZone zone : demandZoneRepository().getZones()) {
+      System.out.println("PopulationSynthesis: Zone " + zone.getId());
+      calculator.calculateDemandData(zone, impedance());
+    }
+  }
 
-	private void printZoneInformation() {
-		for (DemandZone zone : demandZoneRepository().getZones()) {
-			System.out.println("PopulationSynthesis: Zone " + zone.getId() + ", oid=" + zone.getOid());
-		}
-	}
-	
-	protected abstract DemandDataForZoneCalculatorIfc createCalculator(
-			Map<ActivityType, FixedDistributionMatrix> fdMatrices);
+  private void printZoneInformation() {
+    for (DemandZone zone : demandZoneRepository().getZones()) {
+      System.out.println("PopulationSynthesis: Zone " + zone.getId() + ", oid=" + zone.getOid());
+    }
+  }
 
-	private void finishExecution() {
-		try {
-			dataRepository().finishExecution();
-		} catch (IOException cause) {
-			throw new UncheckedIOException(cause);
-		}
-	}
+  protected abstract DemandDataForZoneCalculatorIfc createCalculator(
+      Map<ActivityType, FixedDistributionMatrix> fdMatrices);
 
-	private Map<ActivityType, FixedDistributionMatrix> fixedDistributionMatrices() {
-		System.out.println("Lade Matrizen....");
-		Map<ActivityType, FixedDistributionMatrix> fixedDistributionMatrices = dataRepository()
-				.fixedDistributionMatrices();
-		System.out.println("...geladen!\n");
-		return fixedDistributionMatrices;
-	}
+  private void finishExecution() {
+    try {
+      dataRepository().finishExecution();
+    } catch (IOException cause) {
+      throw new UncheckedIOException(cause);
+    }
+  }
 
-	private void assertFixedDistributionMatrices(
-			Map<ActivityType, FixedDistributionMatrix> inMatrices) {
-		verify(ActivityType.WORK, inMatrices);
-		verify(ActivityType.EDUCATION, inMatrices);
-	}
+  private Map<ActivityType, FixedDistributionMatrix> fixedDistributionMatrices() {
+    System.out.println("Lade Matrizen....");
+    Map<ActivityType, FixedDistributionMatrix> fixedDistributionMatrices = dataRepository()
+        .fixedDistributionMatrices();
+    System.out.println("...geladen!\n");
+    return fixedDistributionMatrices;
+  }
 
-	private void verify(
-			ActivityType activityType, Map<ActivityType, FixedDistributionMatrix> matrices) {
-		if (!matrices.containsKey(activityType)) {
-			throw new IllegalStateException("Fixed distribution matrix missing for " + activityType);
-		}
-	}
+  private void assertFixedDistributionMatrices(
+      Map<ActivityType, FixedDistributionMatrix> inMatrices) {
+    verify(ActivityType.WORK, inMatrices);
+    verify(ActivityType.EDUCATION, inMatrices);
+  }
+
+  private void verify(
+      ActivityType activityType, Map<ActivityType, FixedDistributionMatrix> matrices) {
+    if (!matrices.containsKey(activityType)) {
+      throw new IllegalStateException("Fixed distribution matrix missing for " + activityType);
+    }
+  }
 }
