ee92b258db0619bd31daf21a9b84d3922de4baf0
Lars Briem <lars.briem@kit.edu>
2019-02-07 11:31:00 +0100

    #230 Configurable attributes for ipu
    Clean up.

--- mobitopp/src/test/java/edu/kit/ifv/mobitopp/populationsynthesis/ExampleHouseholdOfPanelData.java
+++ mobitopp/src/test/java/edu/kit/ifv/mobitopp/populationsynthesis/ExampleHouseholdOfPanelData.java
@@ -7,25 +7,19 @@ import edu.kit.ifv.mobitopp.util.panel.HouseholdOfPanelDataId;
 
 public class ExampleHouseholdOfPanelData {
 
-	public static final short year = 2000;
-	public static final int aNumber = 3;
-	public static final int otherNumber = 4;
-	public static final HouseholdOfPanelDataId anId = new HouseholdOfPanelDataId(year, aNumber);
-	public static final HouseholdOfPanelDataId otherId = new HouseholdOfPanelDataId(year, otherNumber);
-	public static final int areaType = 4;
-	public static final int size = 5;
-	public static final int aDomCode = 6;
-	public static final int otherDomCode = 7;
-	public static final int reportingPersons = 8;
-	public static final int minors = 9;
-	public static final int notReportingChildren = 10;
-	public static final int cars = 11;
-	public static final int income = 12;
-	public static final HouseholdOfPanelData household = ExampleHouseholdOfPanelData.household(aDomCode, anId);
-	public static final HouseholdOfPanelData otherHousehold = ExampleHouseholdOfPanelData.household(aDomCode, otherId);
+  public static final short year = 2000;
+  public static final int aNumber = 3;
+  public static final int otherNumber = 4;
+  public static final HouseholdOfPanelDataId anId = new HouseholdOfPanelDataId(year, aNumber);
+  public static final HouseholdOfPanelDataId otherId = new HouseholdOfPanelDataId(year,
+      otherNumber);
+  public static final int aDomCode = 6;
+  public static final int otherDomCode = 7;
+  public static final HouseholdOfPanelData household = household(aDomCode, anId);
+  public static final HouseholdOfPanelData otherHousehold = household(aDomCode, otherId);
 
-	public static HouseholdOfPanelData household(int domCode, HouseholdOfPanelDataId id) {
-		return householdOfPanelData().withId(id).withDomCode(domCode).build();
-	}
+  public static HouseholdOfPanelData household(int domCode, HouseholdOfPanelDataId id) {
+    return householdOfPanelData().withId(id).withDomCode(domCode).build();
+  }
 
 }
