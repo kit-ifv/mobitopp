faa149614705e9a52d339fd3c8f0fe14216f750a
Lars Briem <lars.briem@kit.edu>
2019-02-27 09:30:45 +0100

    #241 configurable attribute names
    Simplify reading of visum networks.
    Remove loading of road network, as it is never used.

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/simulation/ZoneBasedDijkstraRouteChoice.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/simulation/ZoneBasedDijkstraRouteChoice.java
@@ -1,18 +1,16 @@
 package edu.kit.ifv.mobitopp.simulation;
 
-import edu.kit.ifv.mobitopp.routing.Path;
+import java.util.ArrayList;
+
 import edu.kit.ifv.mobitopp.routing.DefaultPath;
-import edu.kit.ifv.mobitopp.routing.Link;
-import edu.kit.ifv.mobitopp.routing.GraphFromVisumNetwork;
-import edu.kit.ifv.mobitopp.routing.Node;
 import edu.kit.ifv.mobitopp.routing.Dijkstra;
+import edu.kit.ifv.mobitopp.routing.GraphFromVisumNetwork;
+import edu.kit.ifv.mobitopp.routing.Link;
+import edu.kit.ifv.mobitopp.routing.Node;
+import edu.kit.ifv.mobitopp.routing.Path;
 import edu.kit.ifv.mobitopp.routing.util.PriorityQueueBasedPQ;
 import edu.kit.ifv.mobitopp.time.Time;
-import edu.kit.ifv.mobitopp.visum.VisumReader;
-import edu.kit.ifv.mobitopp.visum.VisumNetworkReader;
 import edu.kit.ifv.mobitopp.visum.VisumRoadNetwork;
-
-import java.util.ArrayList;
 
 public class ZoneBasedDijkstraRouteChoice
 	implements ZoneBasedRouteChoice {
@@ -21,13 +19,7 @@ public class ZoneBasedDijkstraRouteChoice
 	private final Dijkstra dijkstra = new Dijkstra(new PriorityQueueBasedPQ<>());
 
 
-	public ZoneBasedDijkstraRouteChoice(String visumFile) {
-
-		VisumReader reader = new VisumReader();
-		VisumNetworkReader networkReader = new VisumNetworkReader(reader);
-
-		VisumRoadNetwork network = networkReader.readRoadNetwork(visumFile);
-
+	public ZoneBasedDijkstraRouteChoice(VisumRoadNetwork network) {
 		this.graph = new GraphFromVisumNetwork(network);
 	}
 
