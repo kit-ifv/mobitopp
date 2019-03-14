d8ed22719ecec3dd1555b54b04dac4d5a946d10d
Lars Briem <lars.briem@kit.edu>
2019-03-14 13:39:06 +0100

    Allow column mapping to skip columns and cache header.

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/ColumnMapping.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/ColumnMapping.java
@@ -11,15 +11,32 @@ import edu.kit.ifv.mobitopp.data.Value;
 
 public class ColumnMapping<T> {
 
+  private static final int noOffset = 0;
   private final LinkedHashMap<String, Function<T, Object>> columns;
+  private final int offset;
+  private ArrayList<String> header;
 
-  public ColumnMapping() {
+  public ColumnMapping(int offset) {
     super();
+    this.offset = offset;
     this.columns = new LinkedHashMap<>();
   }
 
+  public ColumnMapping() {
+    this(noOffset);
+  }
+
   public void add(String columnName, Function<T, Object> resolver) {
+    storeColumn(columnName, resolver);
+    resetHeader();
+  }
+
+  private void storeColumn(String columnName, Function<T, Object> resolver) {
     columns.put(columnName, resolver);
+  }
+
+  private void resetHeader() {
+    header = null;
   }
 
   public List<String> prepare(T element) {
@@ -32,11 +49,14 @@ public class ColumnMapping<T> {
   }
 
   public List<String> header() {
-    return new ArrayList<>(columns.keySet());
+    if (null == header) {
+      header = new ArrayList<>(columns.keySet());
+    }
+    return header;
   }
 
   public Value get(String field, List<String> data) {
-    int index = new ArrayList<>(columns.keySet()).indexOf(field);
+    int index = offset + header().indexOf(field);
     return new Value(data.get(index));
   }
 
--- mobitopp/src/test/java/edu/kit/ifv/mobitopp/populationsynthesis/ColumnMappingTest.java
+++ mobitopp/src/test/java/edu/kit/ifv/mobitopp/populationsynthesis/ColumnMappingTest.java
@@ -2,13 +2,16 @@ package edu.kit.ifv.mobitopp.populationsynthesis;
 
 import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.sameInstance;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 import java.util.function.Function;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import edu.kit.ifv.mobitopp.data.Value;
 
@@ -17,17 +20,58 @@ public class ColumnMappingTest {
   @Test
   public void getValueForField() {
     ColumnMapping<Object> columns = new ColumnMapping<>();
-    
+
     columns.add("field1", Function.identity());
     columns.add("field2", Function.identity());
-    
+
     Value firstValue = new Value("first-value");
     Value secondValue = new Value("second-value");
     List<String> fromData = asList(firstValue.toString(), secondValue.toString());
     Value firstField = columns.get("field1", fromData);
     Value secondField = columns.get("field2", fromData);
-    
+
     assertThat(firstField, is(equalTo(firstValue)));
     assertThat(secondField, is(equalTo(secondValue)));
+  }
+
+  @Test
+  void considersOffset() throws Exception {
+    int offset = 1;
+    ColumnMapping<Object> columns = new ColumnMapping<>(offset);
+
+    columns.add("field2", Function.identity());
+
+    Value secondValue = new Value("second-value");
+    List<String> fromData = asList("first-value", secondValue.toString());
+    Value secondField = columns.get("field2", fromData);
+
+    assertThat(secondField, is(equalTo(secondValue)));
+  }
+
+  @Test
+  void cacheHeader() throws Exception {
+    ColumnMapping<Object> columns = new ColumnMapping<>();
+
+    columns.add("field1", Function.identity());
+    columns.add("field2", Function.identity());
+
+    List<String> firstCall = columns.header();
+    List<String> secondCall = columns.header();
+
+    assertThat(firstCall, is(sameInstance(secondCall)));
+  }
+
+  @Test
+  void refreshHeaderOnColumnChange() throws Exception {
+    ColumnMapping<Object> columns = new ColumnMapping<>();
+
+    columns.add("field1", Function.identity());
+    List<String> firstCall = columns.header();
+
+    columns.add("field2", Function.identity());
+    List<String> secondCall = columns.header();
+
+    assertAll(() -> assertThat(firstCall, contains("field1")),
+        () -> assertThat(secondCall, contains("field1", "field2")));
   }
 }
