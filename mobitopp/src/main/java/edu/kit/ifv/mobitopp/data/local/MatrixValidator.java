17890bf0b43212f8c041d6951ba2fdedd5213a32
Lars Briem <lars.briem@kit.edu>
2019-03-26 09:38:39 +0100

    Use separate travel time matrix for ride pooling.

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/data/local/MatrixValidator.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/data/local/MatrixValidator.java
@@ -26,106 +26,108 @@ import edu.kit.ifv.mobitopp.simulation.ActivityType;
 
 public class MatrixValidator {
 
-	private static final TimeSpan wholeDay = TimeSpan.between(0, 23);
+  private static final TimeSpan wholeDay = TimeSpan.between(0, 23);
 
-	private final StoredMatrices matrices;
-	private final File baseFolder;
+  private final StoredMatrices matrices;
+  private final File baseFolder;
 
-	public MatrixValidator(StoredMatrices matrices, File baseFolder) {
-		super();
-		this.matrices = matrices;
-		this.baseFolder = baseFolder;
-	}
+  public MatrixValidator(StoredMatrices matrices, File baseFolder) {
+    super();
+    this.matrices = matrices;
+    this.baseFolder = baseFolder;
+  }
 
-	public void areValid() {
-		verifyCostMatrices();
-		verifyFixedDistributionMatrices();
-		verifyTravelTimeMatrices();
-		verifyFiles();
-	}
+  public void areValid() {
+    verifyCostMatrices();
+    verifyFixedDistributionMatrices();
+    verifyTravelTimeMatrices();
+    verifyFiles();
+  }
 
-	private void verifyFiles() {
-		matrices.forEach(this::validate);
-	}
+  private void verifyFiles() {
+    matrices.forEach(this::validate);
+  }
 
-	private void validate(StoredMatrix matrix) {
-		Validate.files(matrix.file(baseFolder)).doExist();
-	}
+  private void validate(StoredMatrix matrix) {
+    Validate.files(matrix.file(baseFolder)).doExist();
+  }
 
-	private void verifyTravelTimeMatrices() {
-		List<String> missingMatrices = missingTravelTimeMatrices();
-		verify(missingMatrices, "Travel time");
-	}
+  private void verifyTravelTimeMatrices() {
+    List<String> missingMatrices = missingTravelTimeMatrices();
+    verify(missingMatrices, "Travel time");
+  }
 
-	private List<String> missingTravelTimeMatrices() {
-		List<String> missingMatrices = new LinkedList<>();
-		for (TravelTimeMatrixType type : travelTimeTypes()) {
-			TypeMatrices typeMatrices = matrices.travelTimeMatrixFor(type);
-			Consumer<String> matrixCollector = text -> missingMatrices.add(type + " - " + text);
-			collectPerDay(typeMatrices, matrixCollector);
-		}
-		return missingMatrices;
-	}
+  private List<String> missingTravelTimeMatrices() {
+    List<String> missingMatrices = new LinkedList<>();
+    for (TravelTimeMatrixType type : travelTimeTypes()) {
+      TypeMatrices typeMatrices = matrices.travelTimeMatrixFor(type);
+      Consumer<String> matrixCollector = text -> missingMatrices.add(type + " - " + text);
+      collectPerDay(typeMatrices, matrixCollector);
+    }
+    return missingMatrices;
+  }
 
-	private Collection<TravelTimeMatrixType> travelTimeTypes() {
-		return EnumSet.complementOf(EnumSet.of(TravelTimeMatrixType.truck));
-	}
+  private Collection<TravelTimeMatrixType> travelTimeTypes() {
+    return EnumSet
+        .complementOf(EnumSet.of(TravelTimeMatrixType.truck, TravelTimeMatrixType.ride_pooling));
+  }
 
-	private void verifyFixedDistributionMatrices() {
-		List<String> missingMatrices = missingFixedDistributionMatrices();
-		verify(missingMatrices, "Fixed distribution");
-	}
+  private void verifyFixedDistributionMatrices() {
+    List<String> missingMatrices = missingFixedDistributionMatrices();
+    verify(missingMatrices, "Fixed distribution");
+  }
 
-	private List<String> missingFixedDistributionMatrices() {
-		List<String> missingMatrices = new LinkedList<>();
-		for (ActivityType type : fixedDistributionTypes()) {
-			try {
-				matrices.fixedDistributionMatrixFor(type);
-			} catch (IllegalArgumentException cause) {
-				missingMatrices.add(type.name());
-			}
-		}
-		return missingMatrices;
-	}
+  private List<String> missingFixedDistributionMatrices() {
+    List<String> missingMatrices = new LinkedList<>();
+    for (ActivityType type : fixedDistributionTypes()) {
+      try {
+        matrices.fixedDistributionMatrixFor(type);
+      } catch (IllegalArgumentException cause) {
+        missingMatrices.add(type.name());
+      }
+    }
+    return missingMatrices;
+  }
 
-	private Collection<ActivityType> fixedDistributionTypes() {
-		return EnumSet.of(EDUCATION, EDUCATION_OCCUP, EDUCATION_PRIMARY, EDUCATION_SECONDARY,
-				EDUCATION_TERTIARY, WORK);
-	}
+  private Collection<ActivityType> fixedDistributionTypes() {
+    return EnumSet
+        .of(EDUCATION, EDUCATION_OCCUP, EDUCATION_PRIMARY, EDUCATION_SECONDARY, EDUCATION_TERTIARY,
+            WORK);
+  }
 
-	private void verifyCostMatrices() {
-		List<String> missingMatrices = missingCostMatrices();
-		verify(missingMatrices, "Cost");
-	}
+  private void verifyCostMatrices() {
+    List<String> missingMatrices = missingCostMatrices();
+    verify(missingMatrices, "Cost");
+  }
 
-	private void verify(List<String> missingMatrices, String type) {
-		if (missingMatrices.isEmpty()) {
-			return;
-		}
-		throw new IllegalArgumentException(type + " matrices are not specified: " + missingMatrices);
-	}
+  private void verify(List<String> missingMatrices, String type) {
+    if (missingMatrices.isEmpty()) {
+      return;
+    }
+    throw new IllegalArgumentException(type + " matrices are not specified: " + missingMatrices);
+  }
 
-	private List<String> missingCostMatrices() {
-		List<String> missingMatrices = new LinkedList<>();
-		for (CostMatrixType type : CostMatrixType.values()) {
-			TypeMatrices typeMatrices = matrices.costMatrixFor(type);
-			Consumer<String> matrixCollector = text -> missingMatrices.add(type + " - " + text);
-			collectPerDay(typeMatrices, matrixCollector);
-		}
-		return missingMatrices;
-	}
+  private List<String> missingCostMatrices() {
+    List<String> missingMatrices = new LinkedList<>();
+    for (CostMatrixType type : CostMatrixType.values()) {
+      TypeMatrices typeMatrices = matrices.costMatrixFor(type);
+      Consumer<String> matrixCollector = text -> missingMatrices.add(type + " - " + text);
+      collectPerDay(typeMatrices, matrixCollector);
+    }
+    return missingMatrices;
+  }
 
-	private void collectPerDay(TypeMatrices typeMatrices, Consumer<String> matrixCollector) {
-		for (DayType dayType : DayType.values()) {
-			DayTypeMatrices dayTypeMatrices = typeMatrices.at(dayType);
-			if (coversOnlyPartDay(dayTypeMatrices)) {
-				matrixCollector.accept(dayType.name());
-			}
-		}
-	}
+  private void collectPerDay(TypeMatrices typeMatrices, Consumer<String> matrixCollector) {
+    for (DayType dayType : DayType.values()) {
+      DayTypeMatrices dayTypeMatrices = typeMatrices.at(dayType);
+      if (coversOnlyPartDay(dayTypeMatrices)) {
+        matrixCollector.accept(dayType.name());
+      }
+    }
+  }
 
-	private boolean coversOnlyPartDay(DayTypeMatrices dayTypeMatrices) {
-		return !wholeDay.equals(dayTypeMatrices.coveredTime());
-	}
+  private boolean coversOnlyPartDay(DayTypeMatrices dayTypeMatrices) {
+    return !wholeDay.equals(dayTypeMatrices.coveredTime());
+  }
 
 }
