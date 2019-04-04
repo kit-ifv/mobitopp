6395431f2a62861b731d25b0dd9f724267cbe22c
Lars Briem <lars.briem@kit.edu>
2019-04-04 16:10:08 +0200

    Clean up poi categories.

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/visum/POICategories.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/visum/POICategories.java
@@ -1,57 +1,55 @@
 package edu.kit.ifv.mobitopp.visum;
 
+import static java.util.stream.Collectors.toMap;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import edu.kit.ifv.mobitopp.visum.routes.Row;
 
 public class POICategories {
 
-	private final VisumTable categories;
+  private final Map<String, Integer> codeToNumber;
 
-	public POICategories(VisumTable categories) {
+  private POICategories(Map<String, Integer> codeToNumber) {
+    super();
+    this.codeToNumber = codeToNumber;
+  }
 
-		assert null != categories;
-		assert categories.containsAttribute("NR");
-		assert categories.containsAttribute("CODE");
+  public static POICategories from(VisumTable table) {
+    Objects.requireNonNull(table);
+    assert table.containsAttribute("NR");
+    assert table.containsAttribute("CODE");
+    return from(table.rows());
+  }
 
-		this.categories = categories;
-	}
+  public static POICategories from(Collection<Row> rows) {
+    return from(rows.stream());
+  }
 
+  public static POICategories from(Stream<Row> streamedRows) {
+    Map<String, Integer> codeToNumber = streamedRows
+        .collect(toMap(row -> row.get("CODE"), row -> row.valueAsInteger("NR")));
+    return new POICategories(codeToNumber);
+  }
 
+  public boolean containsCode(String code) {
+    return codeToNumber.containsKey(code);
+  }
 
-	public boolean containsCode(String code) {
-		for (int i=0; i < categories.numberOfRows(); i++) {
+  public int numberByCode(String code) {
+    if (codeToNumber.containsKey(code)) {
+      return codeToNumber.get(code);
+    }
+    throw new IllegalArgumentException(String
+        .format("Cannot resolve code %s to number. Available codes are: %s", code,
+            codeToNumber.keySet()));
+  }
 
-			String val = categories.getValue(i, "CODE");
-
-			if (val.equals(code)) { return true; }
-		}
-
-		return false;
-	}
-
-	public int numberByCode(String code) {
-
-		for (int i=0; i < categories.numberOfRows(); i++) {
-
-			String val = categories.getValue(i, "CODE");
-
-			if (val.equals(code)) { 
-				return Integer.parseInt(categories.getValue(i, "NR"));
-			}
-		}
-
-		throw new AssertionError();
-	}
-
-	public boolean containsNumber(int number) {
-
-		for (int i=0; i < categories.numberOfRows(); i++) {
-
-			int val = Integer.parseInt(categories.getValue(i, "NR"));
-
-			if (val == number) { return true; }
-		}
-
-		return false;
-
-	}
+  public boolean containsNumber(int number) {
+    return codeToNumber.containsValue(number);
+  }
 
 }
