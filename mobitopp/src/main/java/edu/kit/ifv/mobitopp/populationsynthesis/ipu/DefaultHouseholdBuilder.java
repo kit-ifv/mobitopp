b12735969d4c0db375bd42f86cd79c73fbbfd856
Lars Briem <lars.briem@kit.edu>
2019-04-04 10:14:03 +0200

    Do not increase hard coded distribution

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/ipu/DefaultHouseholdBuilder.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/ipu/DefaultHouseholdBuilder.java
@@ -35,8 +35,6 @@ public class DefaultHouseholdBuilder implements HouseholdBuilder {
 		currentHousehold = householdCreator.createHousehold(panelHousehold, zone());
 		createSimulatedPeople(panelHousehold);
 		demandZone.actualDemography().incrementHousehold(currentHousehold.getSize());
-		int income = (int) currentHousehold.monthlyIncomeEur();
-    demandZone.actualDemography().increment(StandardAttribute.income, income);
 		return currentHousehold;
 	}
 
