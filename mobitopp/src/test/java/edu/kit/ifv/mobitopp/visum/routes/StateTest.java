2e5063111fc43f77cc1815810dbb45d72fd4a75f
Lars Briem <lars.briem@kit.edu>
2019-04-12 09:37:43 +0200

    Fix broken test.

--- mobitopp/src/test/java/edu/kit/ifv/mobitopp/visum/routes/StateTest.java
+++ mobitopp/src/test/java/edu/kit/ifv/mobitopp/visum/routes/StateTest.java
@@ -1,8 +1,10 @@
 package edu.kit.ifv.mobitopp.visum.routes;
 
 import static java.util.Arrays.asList;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,9 +18,10 @@ public class StateTest {
     RelativeTime time = RelativeTime.ofMinutes(1).plusSeconds(2);
     String serialised = serialised(time);
     Row row = Row
-        .createRow(asList("Z1", serialised),
-            asList("STRECKE\\NACHKNOTEN\\BEZIRKNR", "STRECKE\\T0-IVSYS(P)"));
+        .createRow(asList("Z1", serialised, "1", "2"),
+            asList("STRECKE\\NACHKNOTEN\\BEZIRKNR", "STRECKE\\T0-IVSYS(P)", "VONKNOTNR", "NACHKNOTNR"));
     RouteReader routeReader = mock(RouteReader.class);
+    when(routeReader.getConnectorTime(any())).thenReturn(RelativeTime.ZERO);
     State.intermediateZone.parse(row, routeReader);
 
     verify(routeReader).addZone(new ZoneIdTime(zone, time));
