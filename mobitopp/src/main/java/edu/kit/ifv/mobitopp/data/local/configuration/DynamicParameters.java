82c4498e11de76eb19eec6e0c2df8947b35ec68f
Lars Briem <lars.briem@kit.edu>
2019-04-17 11:18:15 +0200

    Improve exception message.

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/data/local/configuration/DynamicParameters.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/data/local/configuration/DynamicParameters.java
@@ -26,7 +26,7 @@ public class DynamicParameters {
 
 	private void verifyExisting(String parameter) {
 		if (!parameters.containsKey(parameter)) {
-			throw new IllegalArgumentException("Dynamic parameter missing: " + parameter);
+			throw new IllegalArgumentException("Dynamic parameter missing: " + parameter + " containing: " + parameter.toString());
 		}
 	}
 
