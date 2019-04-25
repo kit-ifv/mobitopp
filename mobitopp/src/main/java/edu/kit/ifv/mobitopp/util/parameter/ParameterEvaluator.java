048e36a89599cc54c7f75a641406b47e5b12c72a
Lars Briem <lars.briem@kit.edu>
2019-04-25 12:40:33 +0200

    Treat parameters as double or int.

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/util/parameter/ParameterEvaluator.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/util/parameter/ParameterEvaluator.java
@@ -7,15 +7,25 @@ import javax.script.ScriptException;
 public class ParameterEvaluator {
 
 	public double evaluateAsDouble(String parameter) {
+	  if (parameter.trim().isEmpty()) {
+	    return 0.0d;
+	  }
 		ScriptEngine engine = new ScriptEngineManager().getEngineByName("JavaScript");
 		try {
-			return (double) engine.eval(parameter);
+			Object eval = engine.eval(parameter);
+			if (eval instanceof Double) {
+			  return (double) eval;
+			}
+			return (int) eval;
 		} catch (ScriptException cause) {
 			throw new IllegalArgumentException("Cannot parse parameter: " + parameter);
 		}
 	}
 
 	public int evaluateAsInt(String parameter) {
+    if (parameter.trim().isEmpty()) {
+      return 0;
+    }
 		ScriptEngine engine = new ScriptEngineManager().getEngineByName("JavaScript");
 		try {
 			return (int) engine.eval(parameter);
