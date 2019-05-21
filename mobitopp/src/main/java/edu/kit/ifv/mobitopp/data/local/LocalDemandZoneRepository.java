5729c83306484c6e6c43dc480cebd7a4db19de91
Lars Briem <lars.briem@kit.edu>
2019-05-21 14:21:53 +0200

    #226 Limit number of zones in population synthesis
    Use specified number of zones.

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/data/local/LocalDemandZoneRepository.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/data/local/LocalDemandZoneRepository.java
@@ -1,12 +1,15 @@
 package edu.kit.ifv.mobitopp.data.local;
 
 import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toMap;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
 
 import edu.kit.ifv.mobitopp.data.DemandZone;
 import edu.kit.ifv.mobitopp.data.DemandZoneRepository;
@@ -52,13 +55,21 @@ public class LocalDemandZoneRepository implements DemandZoneRepository {
   }
 
   public static DemandZoneRepository from(
-      ZoneRepository zoneRepository, DemographyData demographyData) {
-    Map<ZoneId, DemandZone> zones = new TreeMap<>();
-    for (Zone zone : zoneRepository.getZones()) {
-      DemandZone demandZone = createZone(zone, demographyData);
-      zones.put(demandZone.getId(), demandZone);
-    }
+      ZoneRepository zoneRepository, DemographyData demographyData, int numberOfZones) {
+    Function<Zone, DemandZone> toDemandZone = zone -> createZone(zone, demographyData);
+    Map<ZoneId, DemandZone> zones = zoneRepository
+        .getZones()
+        .stream()
+        .limit(numberOfZones)
+        .map(toDemandZone)
+        .collect(toMap(DemandZone::getId, Function.identity(), throwingMerger(), TreeMap::new));
     return new LocalDemandZoneRepository(zones, zoneRepository);
+  }
+
+  private static BinaryOperator<DemandZone> throwingMerger() {
+    return (u, v) -> {
+      throw new IllegalStateException(String.format("Duplicate key: %s and %s", u, v));
+    };
   }
 
   private static DemandZone createZone(Zone zone, DemographyData demographyData) {
