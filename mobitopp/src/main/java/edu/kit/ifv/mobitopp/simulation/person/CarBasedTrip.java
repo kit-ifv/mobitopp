649c10b5e4f517297daf0c61bc2be8445d79c972
Lars Briem <lars.briem@kit.edu>
2019-05-29 12:39:51 +0200

    # 261 Car sharing cars are not stopped at the end of a trip
    Unify car based trips.

---
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/simulation/person/CarBasedTrip.java
@@ -0,0 +1,146 @@
+package edu.kit.ifv.mobitopp.simulation.person;
+
+import java.util.Objects;
+
+import edu.kit.ifv.mobitopp.data.Zone;
+import edu.kit.ifv.mobitopp.simulation.BaseTrip;
+import edu.kit.ifv.mobitopp.simulation.Car;
+import edu.kit.ifv.mobitopp.simulation.ImpedanceIfc;
+import edu.kit.ifv.mobitopp.simulation.Location;
+import edu.kit.ifv.mobitopp.simulation.Mode;
+import edu.kit.ifv.mobitopp.simulation.PersonListener;
+import edu.kit.ifv.mobitopp.simulation.Trip;
+import edu.kit.ifv.mobitopp.simulation.TripData;
+import edu.kit.ifv.mobitopp.simulation.activityschedule.ActivityIfc;
+import edu.kit.ifv.mobitopp.simulation.car.CarPosition;
+import edu.kit.ifv.mobitopp.time.Time;
+
+/**
+ * This class should be used as a base class for all tours where the car is reserved for the driver
+ * during the whole tour. It handles parking and releasing behaviour.
+ * 
+ * @author lars.briem@kit.edu
+ */
+public abstract class CarBasedTrip extends BaseTrip {
+
+  private final Class<? extends Car> carType;
+  private final Mode mode;
+
+  public CarBasedTrip(
+      TripData data, SimulationPerson person, Class<? extends Car> carType, Mode mode) {
+    super(data, person);
+    this.carType = carType;
+    this.mode = mode;
+  }
+
+  /**
+   * Allocates a car to be used during this trip. The car can either be the car of the last trip or
+   * a new one.
+   */
+  @Override
+  public void prepareTrip(ImpedanceIfc impedance, Time currentTime) {
+    Objects.requireNonNull(impedance, "impedance");
+    Objects.requireNonNull(currentTime, "currentTime");
+    if (hasParkedCar()) {
+      useParkedCar();
+    } else {
+      useAllocatedCar(impedance, currentTime);
+    }
+  }
+
+  /**
+   * Returns whether the person already has a car from a previous trip or not. 
+   * 
+   * @return true if the person should use the parked car
+   */
+  protected abstract boolean hasParkedCar();
+
+  private void useParkedCar() {
+    assert person().hasParkedCar() : String
+        .format("Person %s has no parked car.", person().getId());
+    assert !person().isCarDriver() : String
+        .format("Person %s is already driving.", person().getId());
+    assertCarTypeOf(person().whichCar());
+    person().takeCarFromParking();
+  }
+  
+  private void assertCarTypeOf(Car car) {
+    assert carType.isInstance(car) : String
+    .format("Car should be of type %s but is %s.", carType, car.getClass());
+  }
+  
+  private void assertMode() {
+    assert mode() == mode : (trip() + "\n" + "prev: " + previousActivity() + "\n" + "next : "
+        + nextActivity() + "\n --- \n" + person().activitySchedule() + "\n");
+  }
+
+  private void useAllocatedCar(ImpedanceIfc impedance, Time currentTime) {
+    Car car = allocateCar(impedance, currentTime);
+    person().useCar(car, currentTime);
+  }
+
+  /**
+   * This method allocates a car for the person.
+   * 
+   * @param impedance access to all impedance types
+   * @param currentTime current simulation time
+   * @return car to be used on this trip
+   */
+  protected abstract Car allocateCar(ImpedanceIfc impedance, Time currentTime);
+
+  /**
+   * @see Trip
+   */
+  @Override
+  public FinishedTrip finish(Time currentDate, PersonListener listener) {
+    if (person().isCarDriver()) {
+      return doFinish(currentDate, listener);
+    }
+    throw new IllegalStateException(
+        String.format("Cannot finish %s trip without car: %s", carType.getName(), trip()));
+  }
+
+  private FinishedTrip doFinish(Time currentDate, PersonListener listener) {
+    FinishedTrip finishedTrip = super.finish(currentDate, listener);
+    assertMode();
+    assert person().isCarDriver();
+    int carId = stopCar(currentDate);
+    FinishedCarTrip finishedCarTrip = new FinishedCarTrip(finishedTrip, carId);
+    notifyBeforeReturn(listener, finishedCarTrip);
+    returnCar(currentDate);
+    return finishedCarTrip;
+  }
+
+  private int stopCar(Time currentDate) {
+    assertMode();
+    Zone zone = nextActivity().zone();
+    Location location = nextActivity().location();
+    CarPosition position = new CarPosition(zone, location);
+    Car car = person().whichCar();
+    car.stop(currentDate, position);
+    return car.id();
+  }
+
+  /**
+   * This method will be called after the car usage has been stop but before the car is returned or
+   * parked.
+   * 
+   * @param listener listener to be notified about the end of a car trip
+   * @param finishedTrip finished trip
+   */
+  protected abstract void notifyBeforeReturn(PersonListener listener, FinishedTrip finishedTrip);
+
+  private void returnCar(Time currentDate) {
+    Zone zone = nextActivity().zone();
+    if (canReturnCar(nextActivity())) {
+      Car car = person().releaseCar(currentDate);
+      car.returnCar(zone);
+    } else {
+      Location location = nextActivity().location();
+      Car car = person().parkCar(zone, location, currentDate);
+      assertCarTypeOf(car);
+    }
+  }
+
+  protected abstract boolean canReturnCar(ActivityIfc nextActivity);
+}
--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/simulation/person/CarSharingFreeFloatingTrip.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/simulation/person/CarSharingFreeFloatingTrip.java
@@ -1,92 +1,42 @@
 package edu.kit.ifv.mobitopp.simulation.person;
 
-import java.util.Objects;
-
 import edu.kit.ifv.mobitopp.data.Zone;
 import edu.kit.ifv.mobitopp.simulation.Car;
 import edu.kit.ifv.mobitopp.simulation.ImpedanceIfc;
-import edu.kit.ifv.mobitopp.simulation.Location;
 import edu.kit.ifv.mobitopp.simulation.Mode;
 import edu.kit.ifv.mobitopp.simulation.PersonListener;
-import edu.kit.ifv.mobitopp.simulation.TripData;
-import edu.kit.ifv.mobitopp.simulation.car.CarPosition;
-import edu.kit.ifv.mobitopp.simulation.BaseTrip;
 import edu.kit.ifv.mobitopp.simulation.Trip;
+import edu.kit.ifv.mobitopp.simulation.TripData;
+import edu.kit.ifv.mobitopp.simulation.activityschedule.ActivityIfc;
 import edu.kit.ifv.mobitopp.simulation.carsharing.CarSharingCar;
 import edu.kit.ifv.mobitopp.time.Time;
 
-public class CarSharingFreeFloatingTrip extends BaseTrip implements Trip {
+public class CarSharingFreeFloatingTrip extends CarBasedTrip implements Trip {
 
   public CarSharingFreeFloatingTrip(TripData trip, SimulationPerson person) {
-    super(trip, person);
+    super(trip, person, CarSharingCar.class, Mode.CARSHARING_FREE);
   }
 
   @Override
-  public void prepareTrip(ImpedanceIfc impedance, Time currentTime) {
-    Objects.requireNonNull(impedance, "impedance");
-    Objects.requireNonNull(currentTime, "currentTime");
+  public boolean hasParkedCar() {
+    return person().hasParkedCar();
+  }
+
+  @Override
+  protected Car allocateCar(ImpedanceIfc impedance, Time currentTime) {
     Zone zone = person().currentActivity().zone();
-    if (person().hasParkedCar()) {
-      usePreviouslyBookedCar(zone);
-    } else {
-      bookCar(currentTime, zone);
-    }
-  }
-
-  private void usePreviouslyBookedCar(Zone zone) {
-    Car car = person().whichCar();
-    assert car != null;
-    assert car instanceof CarSharingCar;
-    assert !zone.carSharing().isFreeFloatingZone((CarSharingCar) car);
-    person().takeCarFromParking();
-  }
-
-  private void bookCar(Time currentTime, Zone zone) {
+    
     assert zone.carSharing().isFreeFloatingCarSharingCarAvailable(person()) : (person());
 
-    Car car = zone.carSharing().bookFreeFloatingCar(person());
-    person().useCar(car, currentTime);
+    return zone.carSharing().bookFreeFloatingCar(person());
   }
   
   @Override
-  public FinishedTrip finish(Time currentDate, PersonListener listener) {
-    if (person().isCarDriver()) {
-      return doFinish(currentDate, listener);
-    }
-    throw new IllegalStateException(String.format("Cannot finish car sharing trip without car: %s" , trip()));
+  protected boolean canReturnCar(ActivityIfc nextActivity) {
+    return nextActivity.zone().carSharing().isFreeFloatingZone((CarSharingCar) person().whichCar());
   }
 
-  public FinishedTrip doFinish(Time currentDate, PersonListener listener) {
-    FinishedTrip finishedTrip = super.finish(currentDate, listener);
-    assert mode() == Mode.CARSHARING_FREE;
-    assert person().isCarDriver();
-    Zone zone = nextActivity().zone();
-    Car car = stopCar(currentDate);
-    returnCar(currentDate, zone);
-    return new FinishedCarTrip(finishedTrip, car.id());
-  }
-
-  public void returnCar(Time currentDate, Zone zone) {
-    if (zone.carSharing().isFreeFloatingZone((CarSharingCar) person().whichCar())) {
-      Car car = person().releaseCar(currentDate);
-      car.returnCar(zone);
-    } else {
-      Location location = nextActivity().location();
-      Car car = person().parkCar(zone, location, currentDate);
-      assert car instanceof CarSharingCar;
-    }
-  }
-
-  private Car stopCar(Time currentDate) {
-    assert mode() == Mode.CARSHARING_FREE : (trip() + "\n" 
-        + "prev: " + previousActivity() + "\n"
-        + "next : " + nextActivity()  + "\n --- \n"
-        + person().activitySchedule() + "\n");
-    Zone zone = nextActivity().zone();
-    Location location = nextActivity().location();
-    CarPosition position = new CarPosition(zone, location);
-    Car car = person().whichCar();
-    car.stop(currentDate, position);
-    return car;
+  @Override
+  protected void notifyBeforeReturn(PersonListener listener, FinishedTrip finishedTrip) {
   }
 }
