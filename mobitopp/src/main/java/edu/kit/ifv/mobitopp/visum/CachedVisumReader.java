e78d05bc2e3caf3a9f48423af85f86bd302bea71
Lars Briem <lars.briem@kit.edu>
2019-06-04 11:48:18 +0200

    #30 Reduce the required heap space needed to parse Visum networks
    Cache descriptions of visum tables in file.

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/visum/CachedVisumReader.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/visum/CachedVisumReader.java
@@ -1,30 +1,96 @@
 package edu.kit.ifv.mobitopp.visum;
 
+import static java.util.Collections.emptyList;
+
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 import edu.kit.ifv.mobitopp.util.file.StreamContent;
-import edu.kit.ifv.mobitopp.visum.routes.StreamReader;
+import edu.kit.ifv.mobitopp.visum.routes.BaseVisumReader;
 
-public final class CachedVisumReader extends StreamReader {
+public final class CachedVisumReader extends BaseVisumReader {
 
   private File file;
   private byte[] storedInput;
+  private final Map<String, ContentDescription> descriptions;
 
   public CachedVisumReader() {
     super();
+    this.descriptions = new HashMap<>();
   }
 
   protected BufferedReader createReader(File file, Charset charset) throws IOException {
     if (null == this.file || !this.file.equals(file)) {
       storedInput = StreamContent.readBytes(file);
       this.file = file;
+      this.descriptions.clear();
     }
     return new BufferedReader(
         new InputStreamReader(new ByteArrayInputStream(storedInput), charset));
+  }
+
+  @Override
+  protected ContentDescription getContentDescription(File file, String tableName) throws IOException {
+    if (!descriptions.isEmpty()) {
+      ContentDescription emptyDescription = new ContentDescription(0, 0, emptyList(), attributeSeparator);
+      return descriptions.getOrDefault(tableName, emptyDescription);
+    }
+    cacheDescriptionsFrom(file);
+    return descriptions.get(tableName);
+  }
+
+  private void cacheDescriptionsFrom(File file) throws IOException {
+    int currentLine = 0;
+    long startOfContent = defaultStart;
+    long endOfContent = currentLine;
+    String currentTable = "";
+    List<String> attributes = new LinkedList<>();
+    try (BufferedReader reader = createReader(file, charset())) {
+      while (reader.ready()) {
+        String line = reader.readLine();
+        if (isContentFinished(startOfContent, line)) {
+          endOfContent = currentLine;
+          ContentDescription contentDescription = new ContentDescription(startOfContent, endOfContent, attributes, attributeSeparator);
+          descriptions.put(currentTable, contentDescription);
+          startOfContent = defaultStart;
+          endOfContent = 0;
+          currentLine++;
+          continue;
+        }
+        currentLine++;
+        if (hasNoContent(line)) {
+          continue;
+        }
+  
+        if (isStartOfTable(line)) {
+          attributes = new LinkedList<>();
+          attributes.addAll(tableAttributes(line));
+          startOfContent = currentLine;
+          currentTable = tableName(line);
+        }
+      }
+      if (startOfContent == endOfContent || 0 == endOfContent) {
+        endOfContent = currentLine;
+      }
+    }
+    if (defaultStart == startOfContent) {
+      ContentDescription contentDescription = new ContentDescription(0, 0, attributes, attributeSeparator);
+      descriptions.put(currentTable, contentDescription);
+      return;
+    }
+    ContentDescription contentDescription = new ContentDescription(startOfContent, endOfContent, attributes, attributeSeparator);
+    descriptions.put(currentTable, contentDescription);
+  }
+  
+  private boolean isStartOfTable(String line) {
+    return line.startsWith("$");
   }
 }
--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/visum/routes/StreamReader.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/visum/routes/StreamReader.java
@@ -5,6 +5,10 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.util.LinkedList;
+import java.util.List;
+
+import edu.kit.ifv.mobitopp.visum.ContentDescription;
 
 public class StreamReader extends BaseVisumReader {
 
@@ -19,5 +23,38 @@ public class StreamReader extends BaseVisumReader {
   @Override
   protected BufferedReader createReader(File routesFile, Charset charset) throws IOException {
     return Files.newBufferedReader(routesFile.toPath(), charset);
+  }
+
+  @Override
+  protected ContentDescription getContentDescription(File file, String tableName) throws IOException {
+    int currentLine = 0;
+    long startOfContent = defaultStart;
+    long endOfContent = currentLine;
+    List<String> attributes = new LinkedList<>();
+    try (BufferedReader reader = createReader(file, charset())) {
+      while (reader.ready()) {
+        String line = reader.readLine();
+        if (isContentFinished(startOfContent, line)) {
+          endOfContent = currentLine;
+          break;
+        }
+        currentLine++;
+        if (hasNoContent(line)) {
+          continue;
+        }
+  
+        if (isStartOfTable(tableName, line)) {
+          attributes.addAll(tableAttributes(line));
+          startOfContent = currentLine;
+        }
+      }
+      if (startOfContent == endOfContent || 0 == endOfContent) {
+        endOfContent = currentLine;
+      }
+    }
+    if (defaultStart == startOfContent) {
+      return new ContentDescription(0, 0, attributes, tableName);
+    }
+    return new ContentDescription(startOfContent, endOfContent, attributes, attributeSeparator);
   }
 }
