c61dc2c879669f5fe0ccf905e2345cf8df0c08c0
Lars Briem <lars.briem@kit.edu>
2019-06-14 10:57:21 +0200

    #274 Parse dynamic parameters in configuration to more primitive types
    Parse to primitive types.
    Add javadoc for class.

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/data/local/configuration/DynamicParameters.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/data/local/configuration/DynamicParameters.java
@@ -6,53 +6,68 @@ import java.util.Map;
 
 import edu.kit.ifv.mobitopp.data.local.Convert;
 
+/**
+ * This class is a wrapper around a map which allows a simple conversion for values into primitive
+ * types.
+ * 
+ * @author bp1110
+ */
 public class DynamicParameters {
 
-	private final Map<String, String> parameters;
+  private final Map<String, String> parameters;
 
-	public DynamicParameters(Map<String, String> parameters) {
-		super();
-		this.parameters = parameters;
-	}
-	
-	public int valueAsInteger(String parameter) {
-		return Integer.parseInt(value(parameter));
-	}
+  public DynamicParameters(Map<String, String> parameters) {
+    super();
+    this.parameters = parameters;
+  }
 
-	public String value(String parameter) {
-		verifyExisting(parameter);
-		return parameters.get(parameter).toString();
-	}
+  public String value(String parameter) {
+    verifyExisting(parameter);
+    return parameters.get(parameter).toString();
+  }
 
-	private void verifyExisting(String parameter) {
-		if (!hasValue(parameter)) {
-			throw new IllegalArgumentException("Dynamic parameter missing: " + parameter + " containing: " + parameters.toString());
-		}
-	}
+  private void verifyExisting(String parameter) {
+    if (!hasValue(parameter)) {
+      throw new IllegalArgumentException(
+          "Dynamic parameter missing: " + parameter + " containing: " + parameters.toString());
+    }
+  }
 
   public boolean hasValue(String parameter) {
     return parameters.containsKey(parameter);
   }
 
-	public double valueAsDouble(String parameter) {
-		return Double.parseDouble(value(parameter));
-	}
+  public long valueAsLong(String parameter) {
+    return Long.parseLong(value(parameter));
+  }
 
-	public boolean valueAsBoolean(String parameter) {
-		return Boolean.parseBoolean(value(parameter));
-	}
-	
-	public File valueAsFile(String parameter) {
-		return Convert.asFile(value(parameter));
-	}
-	
-	public Map<String, String> toMap() {
-	  return Collections.unmodifiableMap(parameters);
-	}
+  public int valueAsInteger(String parameter) {
+    return Integer.parseInt(value(parameter));
+  }
 
-	@Override
-	public String toString() {
-		return getClass().getName() + " [parameters=" + parameters + "]";
-	}
+  public double valueAsDouble(String parameter) {
+    return Double.parseDouble(value(parameter));
+  }
+
+  public float valueAsFloat(String parameter) {
+    return Float.parseFloat(value(parameter));
+  }
+
+  public boolean valueAsBoolean(String parameter) {
+    return Boolean.parseBoolean(value(parameter));
+  }
+
+  public File valueAsFile(String parameter) {
+    return Convert.asFile(value(parameter));
+  }
+
+  public Map<String, String> toMap() {
+    return Collections.unmodifiableMap(parameters);
+  }
+
+  @Override
+  public String toString() {
+    return getClass().getName() + " [parameters=" + parameters + "]";
+  }
 
 }
