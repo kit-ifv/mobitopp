549497645050b6218b7ff898ff2e79ef243ea223
Lars Briem <lars.briem@briemla.de>
2019-09-26 08:12:27 +0200

    #298 Add economical status as household attribute

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/DefaultHouseholdCreator.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/DefaultHouseholdCreator.java
@@ -10,14 +10,17 @@ import edu.kit.ifv.mobitopp.util.panel.HouseholdOfPanelData;
 public class DefaultHouseholdCreator implements HouseholdCreator {
 
 	private final HouseholdLocationSelector householdLocationSelector;
+	private final EconomicalStatusCalculator economicalStatusCalculator;
 	private final ChargePrivatelySelector canChargePrivatelySelector;
 	private int oids;
 
 	public DefaultHouseholdCreator(
 			HouseholdLocationSelector householdLocationSelector,
+			EconomicalStatusCalculator economicalStatusCalculator,
 			ChargePrivatelySelector canChargePrivatelySelector) {
 		super();
 		this.householdLocationSelector = householdLocationSelector;
+		this.economicalStatusCalculator = economicalStatusCalculator;
 		this.canChargePrivatelySelector = canChargePrivatelySelector;
 		oids = 0;
 	}
@@ -25,7 +28,8 @@ public class DefaultHouseholdCreator implements HouseholdCreator {
 	@Override
 	public HouseholdForSetup createHousehold(HouseholdOfPanelData household, Zone zone) {
 		int oid = oids++;
-		HouseholdId id = new HouseholdId(oid, household.id().getYear(), household.id().getHouseholdNumber());
+		HouseholdId id = new HouseholdId(oid, household.id().getYear(),
+				household.id().getHouseholdNumber());
 		int nominalSize = household.size();
 		int domcode = household.domCode();
 		Location location = householdLocationSelector.selectLocation(zone);
@@ -34,9 +38,12 @@ public class DefaultHouseholdCreator implements HouseholdCreator {
 		int totalNumberOfCars = household.numberOfCars();
 		int income = household.income();
 		int incomeClass = household.incomeClass();
+		EconomicalStatus economicalStatus = economicalStatusCalculator
+				.calculateFor(nominalSize, income);
 		boolean canChargePrivately = canChargePrivatelySelector.canChargeAt(zone);
-    return new DefaultHouseholdForSetup(id, nominalSize, domcode, zone, location, numberOfMinors,
-				numberOfNotSimulatedChildren, totalNumberOfCars, income, incomeClass, canChargePrivately);
+		return new DefaultHouseholdForSetup(id, nominalSize, domcode, zone, location, numberOfMinors,
+				numberOfNotSimulatedChildren, totalNumberOfCars, income, incomeClass, economicalStatus,
+				canChargePrivately);
 	}
 
 }
---
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/EconomicalStatus.java
@@ -0,0 +1,7 @@
+package edu.kit.ifv.mobitopp.populationsynthesis;
+
+
+public enum EconomicalStatus {
+	veryLow
+
+}
