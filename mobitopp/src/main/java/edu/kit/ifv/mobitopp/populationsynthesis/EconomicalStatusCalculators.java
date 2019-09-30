c695b549d698935385a1cfd3efb1f3cf6ceb3d18
Lars Briem <lars.briem@kit.edu>
2019-09-30 10:01:40 +0200

    #298 Add economical status as household attribute
    Parse input data.

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/EconomicalStatusCalculators.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/EconomicalStatusCalculators.java
@@ -1,9 +1,23 @@
 package edu.kit.ifv.mobitopp.populationsynthesis;
 
+import java.io.File;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.util.TreeMap;
+
+import org.assertj.core.util.Files;
+
+import edu.kit.ifv.mobitopp.data.demand.RangeDistributionIfc;
 
 public class EconomicalStatusCalculators {
 
-	public static class MiD2017 implements EconomicalStatusCalculator {
+	public static class DefaultEconomicalStatusCalculator implements EconomicalStatusCalculator {
+
+		private TreeMap<Double, RangeDistributionIfc> distributions;
+
+		public DefaultEconomicalStatusCalculator(TreeMap<Double, RangeDistributionIfc> distributions) {
+			this.distributions = distributions;
+		}
 
 		@Override
 		public EconomicalStatus calculateFor(int nominalSize, int income) {
@@ -12,7 +26,15 @@ public class EconomicalStatusCalculators {
 			
 	}
 
+	private static final String mid2017File = "economical-status-mid2017.csv";
+
 	public static EconomicalStatusCalculator mid2017() {
-		return new MiD2017();
+		try {
+		File file = new File(EconomicalStatusCalculators.class.getResource(mid2017File).toURI());
+		TreeMap<Double, RangeDistributionIfc> distributions = new EconomicalStatusDistributionParser().parse(Files.contentOf(file, Charset.defaultCharset()));
+		return new DefaultEconomicalStatusCalculator(distributions);
+		} catch (URISyntaxException cause) {
+			throw new RuntimeException(cause);
+		}
 	}
 }
