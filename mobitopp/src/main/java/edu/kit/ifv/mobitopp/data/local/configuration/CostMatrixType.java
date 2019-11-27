cdac0bc5e2000df2dc23ea160be43fdb9af23f37
Lars Briem <lars.briem@kit.edu>
2019-11-27 13:32:55 +0100

    #305 Create IPU based example
    Add Bikesharing cost matrix.

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/data/local/configuration/CostMatrixType.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/data/local/configuration/CostMatrixType.java
@@ -1,7 +1,18 @@
 package edu.kit.ifv.mobitopp.data.local.configuration;
 
-
 public enum CostMatrixType implements MatrixType {
 
-	car, publictransport, taxi, truck, park_and_ride, ride_hailing, premium_ride_hailing, ride_pooling, parking, distance, parkingstress, constant;
+	bikesharing,
+	car,
+	publictransport,
+	taxi,
+	truck,
+	park_and_ride,
+	ride_hailing,
+	premium_ride_hailing,
+	ride_pooling,
+	parking,
+	distance,
+	parkingstress,
+	constant;
 }
