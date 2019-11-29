17964be8fe2728b1960ebd2e5a4c702453e3d84a
Lars Briem <lars.briem@kit.edu>
2019-11-29 16:27:24 +0100

    #307 Allow dynamic modes
    Extract standard choice sets.

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/simulation/Mode.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/simulation/Mode.java
@@ -1,72 +1,27 @@
 package edu.kit.ifv.mobitopp.simulation;
 
-import java.util.Collections;
-import java.util.EnumSet;
 import java.util.List;
 import java.util.Set;
 
-
-
 public interface Mode {
-	
-	public static final Set<Mode>	CHOICE_SET_FULL = Collections.unmodifiableSet(EnumSet.of(
-																														StandardMode.CAR,
-																														StandardMode.PASSENGER,
-																														StandardMode.BIKE,
-																														StandardMode.PEDESTRIAN,
-																														StandardMode.PUBLICTRANSPORT
-																									));
-	public static final Set<Mode> CHOICE_SET_FLEXIBLE = Collections.unmodifiableSet(EnumSet.of(
-																														StandardMode.PASSENGER,
-																														StandardMode.PEDESTRIAN,
-																														StandardMode.PUBLICTRANSPORT));
-	public static final Set<Mode> CHOICE_SET_WITHOUT_PASSENGER = Collections.unmodifiableSet(EnumSet.of(
-																														StandardMode.CAR,
-																														StandardMode.BIKE,
-																														StandardMode.PEDESTRIAN,
-																														StandardMode.PUBLICTRANSPORT));
 
-	public static final Set<Mode> CHOICE_SET_SCHAUFENSTER = Collections.unmodifiableSet(EnumSet.of(
-																														StandardMode.CAR,
-																														StandardMode.PASSENGER,
-																														StandardMode.BIKE,
-																														StandardMode.PEDESTRIAN,
-																														StandardMode.PUBLICTRANSPORT,
-																														StandardMode.CARSHARING_STATION,
-																														StandardMode.CARSHARING_FREE,
-																														StandardMode.PEDELEC,
-																														StandardMode.BIKESHARING
-																				));
-
-	public static final Set<Mode> FIXED_MODES = Collections.unmodifiableSet(EnumSet.of(
-																														StandardMode.CAR,
-																														StandardMode.BIKE,
-																														StandardMode.CARSHARING_STATION,
-																														StandardMode.PEDELEC
-																				));
-
-	public static final Set<Mode> MODESET_CAR_BIKE_WALK_PT = Collections.unmodifiableSet(EnumSet.of(
-																													StandardMode.CAR,
-																													StandardMode.BIKE,
-																													StandardMode.PEDESTRIAN,
-																													StandardMode.PUBLICTRANSPORT
-																											));
-	public static final Set<Mode> MODESET_CAR_ONLY = Collections.unmodifiableSet(EnumSet.of(StandardMode.CAR));
-	public static final Set<Mode> MODESET_BIKE_ONLY = Collections.unmodifiableSet(EnumSet.of(StandardMode.BIKE));
-	public static final Set<Mode> MODESET_WALK_PT = Collections.unmodifiableSet(EnumSet.of(
-																													StandardMode.PEDESTRIAN,
-																													StandardMode.PUBLICTRANSPORT));
-
-	public static final List<Set<Mode>> MODESET_VALUES = Collections
-			.unmodifiableList(
-					List.of(MODESET_CAR_BIKE_WALK_PT, MODESET_CAR_ONLY, MODESET_BIKE_ONLY, MODESET_WALK_PT));
+	public static final Set<Mode> CHOICE_SET_FULL = StandardChoiceSet.CHOICE_SET_FULL;
+	public static final Set<Mode> CHOICE_SET_FLEXIBLE = StandardChoiceSet.CHOICE_SET_FLEXIBLE;
+	public static final Set<Mode> CHOICE_SET_WITHOUT_PASSENGER = StandardChoiceSet.CHOICE_SET_WITHOUT_PASSENGER;
+	public static final Set<Mode> CHOICE_SET_SCHAUFENSTER = StandardChoiceSet.CHOICE_SET_SCHAUFENSTER;
+	public static final Set<Mode> FIXED_MODES = StandardChoiceSet.FIXED_MODES;
+	public static final Set<Mode> MODESET_CAR_BIKE_WALK_PT = StandardChoiceSet.MODESET_CAR_BIKE_WALK_PT;
+	public static final Set<Mode> MODESET_CAR_ONLY = StandardChoiceSet.MODESET_CAR_ONLY;
+	public static final Set<Mode> MODESET_BIKE_ONLY = StandardChoiceSet.MODESET_BIKE_ONLY;
+	public static final Set<Mode> MODESET_WALK_PT = StandardChoiceSet.MODESET_WALK_PT;
+	public static final List<Set<Mode>> MODESET_VALUES = StandardChoiceSet.MODESET_VALUES;
 
 	public int getTypeAsInt();
 
 	public boolean isFlexible();
-	
+
 	public boolean isDefined();
-	
+
 	public boolean usesCarAsDriver();
 
 	public static Set<Mode> exclusive(Mode mode) {
