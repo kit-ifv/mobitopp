27681884488fab44b73de7fee53508eee8aa3b39
Lars Briem <lars.briem@kit.edu>
2020-01-09 15:17:17 +0100

    Add passenger as matrix type and provide interface for transit pass.

---
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/commutation/CommutationTicketModelOwnershipModel.java
@@ -0,0 +1,9 @@
+package edu.kit.ifv.mobitopp.populationsynthesis.commutation;
+
+import edu.kit.ifv.mobitopp.populationsynthesis.PersonBuilder;
+
+public interface CommutationTicketModelOwnershipModel {
+
+	boolean hasCommutationTicket(PersonBuilder person);
+
+}
