479786a5939863d110dcc5a2144eee658d7dd9e9
Lars Briem <lars.briem@kit.edu>
2020-08-14 08:36:23 +0200

    #339 Poi based opportunity location selector
    Parse input file only once.

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/opportunities/PoiBasedOpportunityLocationSelector.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/opportunities/PoiBasedOpportunityLocationSelector.java
@@ -1,41 +1,44 @@
 package edu.kit.ifv.mobitopp.populationsynthesis.opportunities;
 
+import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
 
 import java.io.File;
+import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 
 import edu.kit.ifv.mobitopp.data.ZoneId;
 import edu.kit.ifv.mobitopp.simulation.ActivityType;
 import edu.kit.ifv.mobitopp.simulation.Location;
 import edu.kit.ifv.mobitopp.simulation.opportunities.Opportunity;
 import edu.kit.ifv.mobitopp.util.dataimport.CsvFile;
-import edu.kit.ifv.mobitopp.util.dataimport.Row;
 
 public class PoiBasedOpportunityLocationSelector implements OpportunityLocationSelector {
 
-	private final File inputFile;
-	private final PoiParser parser;
+	private final List<Opportunity> opportunities;
 
-	public PoiBasedOpportunityLocationSelector(File inputFile, PoiParser parser) {
+	public PoiBasedOpportunityLocationSelector(List<Opportunity> opportunities) {
 		super();
-		this.inputFile = inputFile;
-		this.parser = parser;
+		this.opportunities = opportunities;
+	}
+
+	public static OpportunityLocationSelector create(File inputFile, PoiParser parser) {
+		List<Opportunity> opportunities = CsvFile
+				.createFrom(inputFile)
+				.stream()
+				.map(parser::parse)
+				.collect(toList());
+		return new PoiBasedOpportunityLocationSelector(opportunities);
 	}
 
 	@Override
 	public Map<Location, Integer> createLocations(
 			ZoneId zone, ActivityType activityType, Integer total_opportunities) {
-		return pois()
-				.map(parser::parse)
+		return opportunities
+				.stream()
 				.filter(opportunity -> zone.equals(opportunity.zone()))
 				.filter(opportunity -> activityType.equals(opportunity.activityType()))
 				.collect(toMap(Opportunity::location, Opportunity::attractivity));
-	}
-
-	Stream<Row> pois() {
-		return CsvFile.createFrom(inputFile).stream();
 	}
 
 }
--- mobitopp/src/test/java/edu/kit/ifv/mobitopp/populationsynthesis/opportunities/PoiBasedOpportunityLocationSelectorTest.java
+++ mobitopp/src/test/java/edu/kit/ifv/mobitopp/populationsynthesis/opportunities/PoiBasedOpportunityLocationSelectorTest.java
@@ -1,14 +1,11 @@
 package edu.kit.ifv.mobitopp.populationsynthesis.opportunities;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 
 import java.awt.geom.Point2D;
 import java.io.File;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,7 +17,6 @@ import edu.kit.ifv.mobitopp.populationsynthesis.SynthesisContext;
 import edu.kit.ifv.mobitopp.simulation.ActivityType;
 import edu.kit.ifv.mobitopp.simulation.Location;
 import edu.kit.ifv.mobitopp.simulation.opportunities.Opportunity;
-import edu.kit.ifv.mobitopp.util.dataimport.Row;
 
 @ExtendWith(MockitoExtension.class)
 public class PoiBasedOpportunityLocationSelectorTest {
@@ -39,23 +35,14 @@ public class PoiBasedOpportunityLocationSelectorTest {
 		ActivityType activityType = ActivityType.WORK;
 		Integer total_opportunities = 1;
 
-		when(parser.parse(any()))
-				.thenReturn(new Opportunity(zone, activityType, location, total_opportunities));
-
-		OpportunityLocationSelector selector = createSelector();
+		OpportunityLocationSelector selector = createSelector(new Opportunity(zone, activityType, location, total_opportunities));
 		Map<Location, Integer> locations = selector
 				.createLocations(zone, activityType, total_opportunities);
 
 		assertThat(locations).containsEntry(location, total_opportunities);
 	}
 
-	private PoiBasedOpportunityLocationSelector createSelector() {
-		return new PoiBasedOpportunityLocationSelector(inputFile, parser) {
-
-			@Override
-			Stream<Row> pois() {
-				return Stream.of(Row.createRow(List.of(), List.of()));
-			}
-		};
+	private PoiBasedOpportunityLocationSelector createSelector(Opportunity opportunity) {
+		return new PoiBasedOpportunityLocationSelector(List.of(opportunity));
 	}
 }
