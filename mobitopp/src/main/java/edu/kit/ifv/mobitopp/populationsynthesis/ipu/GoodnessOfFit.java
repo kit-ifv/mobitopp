1bcbeb8f6ca59a3161272e4ca0470e497b134885
Lars Briem <lars.briem@kit.edu>
2020-10-15 15:35:10 +0200

    #345 Regional Ipu does not scale for larger populations
    Reduce duplication

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/ipu/GoodnessOfFit.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/ipu/GoodnessOfFit.java
@@ -1,7 +1,6 @@
 package edu.kit.ifv.mobitopp.populationsynthesis.ipu;
 
-
-public class GoodnessOfFit {
+public class GoodnessOfFit implements WeightedHouseholdsConsumer {
 
   private double goodnessOfFit;
   private int count;
@@ -11,7 +10,9 @@ public class GoodnessOfFit {
     count = 0;
   }
 
-  public void add(double totalWeight, double requestedWeight) {
+  public void process(
+      int offset, int numberOfWeightsPerPart, int absoluteAttributeIndex, double requestedWeight,
+      double totalWeight) {
     double goodness = Math.abs(totalWeight - requestedWeight) / requestedWeight;
     if (Double.isFinite(goodness)) {
       goodnessOfFit += goodness;
