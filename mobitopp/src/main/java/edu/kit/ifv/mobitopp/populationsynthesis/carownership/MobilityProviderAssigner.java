56a7b4b145996c9ed346225e8f14a13716ac3a4b
Lars Briem <lars.briem@kit.edu>
2020-11-25 09:52:52 +0100

    #369 Allow setting of mobility provider membership on single company
    Add default step to add mobility provider.

---
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/populationsynthesis/carownership/MobilityProviderAssigner.java
@@ -0,0 +1,25 @@
+package edu.kit.ifv.mobitopp.populationsynthesis.carownership;
+
+import java.util.Map;
+import java.util.function.Consumer;
+
+import edu.kit.ifv.mobitopp.populationsynthesis.PersonBuilder;
+
+public class MobilityProviderAssigner implements Consumer<PersonBuilder> {
+
+  private final Map<String, MobilityProviderCustomerModel> models;
+
+  public MobilityProviderAssigner(final Map<String, MobilityProviderCustomerModel> models) {
+    this.models = models;
+  }
+
+  @Override
+  public void accept(final PersonBuilder person) {
+    this.models.forEach((company, model) -> assign(person, model, company));
+  }
+
+  private void assign(PersonBuilder person, MobilityProviderCustomerModel model, String company) {
+    person.setMobilityProviderMembership(company, model.estimateCustomership(person));
+  }
+
+}
