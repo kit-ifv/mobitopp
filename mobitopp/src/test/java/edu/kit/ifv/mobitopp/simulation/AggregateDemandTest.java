e770c2c1988f378e058a819edc5431d47ba1b761
Kuebler Jelle IfV <jelle.kuebler@kit.edu>
2020-12-15 11:30:18 +0100

    Fixed AggregateDemandTest/AggregateDemandOfStartedTripsTest
    Removed IntegerMatrix.equals as it was only used for testing purposes
    Replaced mockito's verify check by assertThat check

--- mobitopp/src/test/java/edu/kit/ifv/mobitopp/simulation/AggregateDemandTest.java
+++ mobitopp/src/test/java/edu/kit/ifv/mobitopp/simulation/AggregateDemandTest.java
@@ -1,9 +1,8 @@
 package edu.kit.ifv.mobitopp.simulation;
 
 import static java.util.Arrays.asList;
-import static org.mockito.ArgumentMatchers.any;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.awt.geom.Point2D;
@@ -16,33 +15,40 @@ import edu.kit.ifv.mobitopp.data.IntegerMatrix;
 import edu.kit.ifv.mobitopp.data.Zone;
 import edu.kit.ifv.mobitopp.data.ZoneId;
 import edu.kit.ifv.mobitopp.simulation.person.FinishedTrip;
+import lombok.Setter;
 
 public class AggregateDemandTest {
 
-  @Test
-  void aggregateTrips() throws Exception {
-    @SuppressWarnings("unchecked")
-    Consumer<IntegerMatrix> writer = mock(Consumer.class);
-    int matrixColumn = 0;
-    ZoneId zoneId = new ZoneId("11", matrixColumn);
-    List<ZoneId> oids = asList(zoneId);
-    AggregateDemand aggregateDemand = new AggregateDemand(writer, oids);
+	@Test
+	void aggregateTrips() throws Exception {
+		TestData data = new TestData();
 
-    Person person = mock(Person.class);
-    FinishedTrip trip = mock(FinishedTrip.class);
-    Zone zone = mock(Zone.class);
-    when(zone.getId()).thenReturn(zoneId);
-    Location location = new Location(new Point2D.Double(), 0, 0);
-    ZoneAndLocation zoneLocation = new ZoneAndLocation(zone, location);
-    when(trip.origin()).thenReturn(zoneLocation);
-    when(trip.destination()).thenReturn(zoneLocation);
+		int matrixColumn = 0;
+		ZoneId zoneId = new ZoneId("11", matrixColumn);
+		List<ZoneId> oids = asList(zoneId);
+		AggregateDemand aggregateDemand = new AggregateDemand(data::setMatrix, oids);
 
-    aggregateDemand.notifyEndTrip(person, trip);
+		Person person = mock(Person.class);
+		FinishedTrip trip = mock(FinishedTrip.class);
+		Zone zone = mock(Zone.class);
+		when(zone.getId()).thenReturn(zoneId);
+		Location location = new Location(new Point2D.Double(), 0, 0);
+		ZoneAndLocation zoneLocation = new ZoneAndLocation(zone, location);
+		when(trip.origin()).thenReturn(zoneLocation);
+		when(trip.destination()).thenReturn(zoneLocation);
 
-    aggregateDemand.notifyFinishSimulation();
+		aggregateDemand.notifyEndTrip(person, trip);
+		aggregateDemand.notifyFinishSimulation();
 
-    IntegerMatrix matrix = new IntegerMatrix(asList(zoneId));
-    matrix.set(matrixColumn, matrixColumn, 1);
-    verify(writer).accept(matrix);
-  }
+		assertThat(data.matrix.get(matrixColumn, matrixColumn)).isEqualTo(1);
+	}
+	
+	@Setter
+	private final class TestData {
+
+		private IntegerMatrix matrix;
+
+	}
+
+
 }
