798ce1b4f0ac3761d1bed17194f9f7b655a1115d
Lars Briem <lars.briem@kit.edu>
2021-02-15 11:33:17 +0100

    #401 Monomodal carsharing is only available at home
    Fix carsharing on the road.

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/simulation/modeChoice/ModeAvailabilityModelAddingCarsharing.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/simulation/modeChoice/ModeAvailabilityModelAddingCarsharing.java
@@ -13,76 +13,75 @@ import edu.kit.ifv.mobitopp.simulation.StandardMode;
 import edu.kit.ifv.mobitopp.simulation.activityschedule.ActivityIfc;
 import edu.kit.ifv.mobitopp.simulation.carsharing.CarSharingCar;
 
-public class ModeAvailabilityModelAddingCarsharing
-	extends BasicModeAvailabilityModel
-	implements ModeAvailabilityModel
-{
+public class ModeAvailabilityModelAddingCarsharing extends BasicModeAvailabilityModel
+	implements ModeAvailabilityModel {
 
-	public ModeAvailabilityModelAddingCarsharing(
-		ImpedanceIfc impedance
-	) {
+	public ModeAvailabilityModelAddingCarsharing(ImpedanceIfc impedance) {
 		super(impedance);
 	}
 
 	@Override
-	public Set<Mode> availableModes(
-		Person person,
-		Zone currentZone,
-		ActivityIfc previousActivity,
-		Collection<Mode> allModes
-	) {
+	public Set<Mode> availableModes(Person person, Zone currentZone, ActivityIfc previousActivity,
+		Collection<Mode> allModes) {
 
 		Set<Mode> choiceSet = new LinkedHashSet<Mode>(
-				super.availableModes(person, currentZone, previousActivity, allModes));
+			super.availableModes(person, currentZone, previousActivity, allModes));
 
 		assert choiceSet != null;
 
 		if (isAtHome(previousActivity)) {
 
-			if ( person.hasDrivingLicense() && person.household().getNumberOfAvailableCars() == 0) {
+			if (person.hasDrivingLicense() && person.household().getNumberOfAvailableCars() == 0) {
 
-				if( currentZone.carSharing().isStationBasedCarSharingCarAvailable(person)) {
+				if (currentZone.carSharing().isStationBasedCarSharingCarAvailable(person)) {
 					choiceSet.add(StandardMode.CARSHARING_STATION);
 				}
 
-				if( currentZone.carSharing().isFreeFloatingCarSharingCarAvailable(person)) {
+				if (currentZone.carSharing().isFreeFloatingCarSharingCarAvailable(person)) {
 					choiceSet.add(StandardMode.CARSHARING_FREE);
 				}
 			}
 
-			if(previousActivity.isModeSet()
-					&& previousActivity.mode()==StandardMode.CARSHARING_FREE && person.hasParkedCar()) {
+			if (previousActivity.isModeSet()
+				&& previousActivity.mode() == StandardMode.CARSHARING_FREE
+				&& person.hasParkedCar()) {
 
-				assert !currentZone.carSharing().isFreeFloatingZone((CarSharingCar)person.whichCar());
+				assert !currentZone
+					.carSharing()
+					.isFreeFloatingZone((CarSharingCar) person.whichCar());
 
 				choiceSet = Collections.singleton(StandardMode.CARSHARING_FREE);
-			} 
+			}
 		} else {
 
 			Mode previousMode = previousActivity.mode();
 
-			if(previousMode==StandardMode.CARSHARING_STATION) {
+			if (previousMode == StandardMode.CARSHARING_STATION) {
 
-					choiceSet = Collections.singleton(StandardMode.CARSHARING_STATION);
+				choiceSet = Collections.singleton(StandardMode.CARSHARING_STATION);
 
-			} else if(previousMode==StandardMode.CARSHARING_FREE) {
+			} else if (previousMode == StandardMode.CARSHARING_FREE) {
 
-				if(person.hasParkedCar()) {
-	
-					assert !currentZone.carSharing().isFreeFloatingZone((CarSharingCar)person.whichCar());
-	
+				if (person.hasParkedCar()) {
+
+					assert !currentZone
+						.carSharing()
+						.isFreeFloatingZone((CarSharingCar) person.whichCar());
+
 					choiceSet = Collections.singleton(StandardMode.CARSHARING_FREE);
 
-				} 
-				else {
+				} else {
 					choiceSet.remove(StandardMode.CAR);
 					choiceSet.remove(StandardMode.BIKE);
-	
-					if(currentZone.carSharing().isFreeFloatingCarSharingCarAvailable(person)) {
+
+					if (currentZone.carSharing().isFreeFloatingCarSharingCarAvailable(person)) {
 						choiceSet.add(StandardMode.CARSHARING_FREE);
 					}
 				}
-			} 
+			} else if (person.hasDrivingLicense()
+				&& currentZone.carSharing().isFreeFloatingCarSharingCarAvailable(person)) {
+				choiceSet.add(StandardMode.CARSHARING_FREE);
+			}
 		}
 
 		return choiceSet;
