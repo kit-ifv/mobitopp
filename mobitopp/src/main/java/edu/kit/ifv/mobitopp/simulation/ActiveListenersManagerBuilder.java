56d3314cf309be1689e5beb41f094ebd5faa45cf
jelle.kuebler <jelle.kuebler@kit.edu>
2021-02-17 13:48:35 +0100

    Added custom time slices to demand aggregation
    ActiveListenersManagerBuilder#addTimeSlicesBetwee
    adds listeners for aggregating demand during a certain time slice on a
    given set of weekdays
    
    Therefore AggregateDemandFactory provides a new method newListener
    to create a new AggregateDemandOfStartedTrips with the according filters
    and matrix writer for a time slice longer than one hour and an arbitrary
    set of weekdays.

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/simulation/ActiveListenersManagerBuilder.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/simulation/ActiveListenersManagerBuilder.java
@@ -10,21 +10,24 @@ import java.util.Set;
 import edu.kit.ifv.mobitopp.data.ZoneId;
 import edu.kit.ifv.mobitopp.time.DayOfWeek;
 import edu.kit.ifv.mobitopp.time.Time;
+import edu.kit.ifv.mobitopp.util.dataexport.AbstractMatrixPrinter;
 import edu.kit.ifv.mobitopp.util.dataexport.ZipMatrixPrinterDecorator;
+import lombok.extern.slf4j.Slf4j;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class ActiveListenersManagerBuilder is a fluent builder for
  * {@link ActiveListenersManager}.
  */
+@Slf4j
 public class ActiveListenersManagerBuilder {
 
-	private int threads = WrittenConfiguration.defaulThreadCount;
-	private DemandSimulatorPassenger simulator = null;
-	private boolean actionsAtStart = false;
+	private int threads;
+	private DemandSimulatorPassenger simulator;
+	private boolean actionsAtStart;
 	private final Collection<Entry<Time, PersonListener>> activations;
 	private final Collection<Entry<Time, PersonListener>> deactivations;
 	private final Collection<Entry<Time, Runnable>> actions;
+	private AbstractMatrixPrinter matrixPrinter;
 
 	/**
 	 * Instantiates a new {@link ActiveListenersManagerBuilder}.
@@ -33,6 +36,11 @@ public class ActiveListenersManagerBuilder {
 		this.activations = new ArrayList<>();
 		this.deactivations = new ArrayList<>();
 		this.actions = new ArrayList<>();
+
+		this.threads = WrittenConfiguration.defaulThreadCount;
+		this.simulator = null;
+		this.actionsAtStart = false;
+		this.matrixPrinter = new ZipMatrixPrinterDecorator();
 	}
 
 	/**
@@ -110,6 +118,19 @@ public class ActiveListenersManagerBuilder {
 	}
 
 	/**
+	 * Specifies that the following (matrix output) added to the schedule use the
+	 * given {@link AbstractMatrixPrinter}.
+	 *
+	 * @param matrixPrinter the {@link AbstractMatrixPrinter} to be used for the
+	 *                      next actions
+	 * @return this {@link ActiveListenersManagerBuilder}
+	 */
+	public ActiveListenersManagerBuilder useMatrixPrinter(AbstractMatrixPrinter matrixPrinter) {
+		this.matrixPrinter = matrixPrinter;
+		return this;
+	}
+
+	/**
 	 * Adds {@link AggregateDemandOfStartedTrips listeners} to the
 	 * {@link ActiveListenersManager} to be built that are active during the week
 	 * for every mode in the given choice set and every hour of the day.
@@ -123,8 +144,7 @@ public class ActiveListenersManagerBuilder {
 	public ActiveListenersManagerBuilder addHourlyWeekdayListeners(List<ZoneId> zoneIds,
 		Set<Mode> choiceSet, SimulationContext context) {
 
-		AggregateDemandFactory factory = new AggregateDemandFactory(context,
-			new ZipMatrixPrinterDecorator());
+		AggregateDemandFactory factory = new AggregateDemandFactory(context, this.matrixPrinter);
 
 		for (Mode mode : choiceSet) {
 			for (int hour = 0; hour < 24; hour++) {
@@ -155,15 +175,14 @@ public class ActiveListenersManagerBuilder {
 	 *
 	 * @param zoneIds   the zone ids for which the demand will be aggregated
 	 * @param choiceSet the set of modes for which listeners will be created
-	 * @param day the day of week on which demand will be aggregated
+	 * @param day       the day of week on which demand will be aggregated
 	 * @param context   the simulation context
 	 * @return this {@link ActiveListenersManagerBuilder}
 	 */
 	public ActiveListenersManagerBuilder addHourlyDayListeners(List<ZoneId> zoneIds,
 		Set<Mode> choiceSet, DayOfWeek day, SimulationContext context) {
 
-		AggregateDemandFactory factory = new AggregateDemandFactory(context,
-			new ZipMatrixPrinterDecorator());
+		AggregateDemandFactory factory = new AggregateDemandFactory(context, this.matrixPrinter);
 
 		for (int hour = 0; hour < 24; hour++) {
 			Time start = Time.start.plusDays(day.getTypeAsInt()).plusHours(hour);
@@ -183,6 +202,64 @@ public class ActiveListenersManagerBuilder {
 	}
 
 	/**
+	 * Adds {@link AggregateDemandOfStartedTrips listeners} to the
+	 * {@link ActiveListenersManager} to be built that are active during a time
+	 * slice between the given hours on the given days for every mode in the given
+	 * choice.
+	 * 
+	 * The upper bound timeTo is exclusive. Listeners are generated for all
+	 * consecutive time slices that fit in [timeFrom, timeTo].
+	 *
+	 * @param zoneIds   the zone ids for which the demand will be aggregated
+	 * @param choiceSet the set of modes for which listeners will be created
+	 * @param days      the days of week on which demand will be aggregated
+	 * @param context   the context
+	 * @param hourFrom  the hour from which on demand will be aggregated (inclusive)
+	 * @param hourTo    the hour until which on demand will be aggregated
+	 *                  (exclusive)
+	 * @param timeSlice the duration of a time slice during which demand will be
+	 *                  aggregated
+	 * @return this {@link ActiveListenersManagerBuilder}
+	 */
+	public ActiveListenersManagerBuilder addTimeSlicesBetween(List<ZoneId> zoneIds,
+		Set<Mode> choiceSet, Collection<DayOfWeek> days, SimulationContext context, int hourFrom,
+		int hourTo, int timeSlice) {
+
+		if ((hourTo - hourFrom) % timeSlice != 0) {
+			log	.warn("The given interval [{},{})"
+				+ " is not devisible by {} hour time slices. "
+				+ "The remaining {} hours are not covered when aggregating demand!",
+				hourFrom, hourTo, timeSlice, (hourTo - hourFrom) % timeSlice);
+		}
+
+		AggregateDemandFactory factory = new AggregateDemandFactory(context, this.matrixPrinter);
+
+		int lastDay = days.stream().mapToInt(DayOfWeek::getTypeAsInt).max().orElseGet(() -> 6);
+
+		for (Mode mode : choiceSet) {
+			for (int hour = hourFrom; hour + timeSlice <= hourTo; hour += timeSlice) {
+
+				AggregateDemandOfStartedTrips agg = factory
+					.newListener(zoneIds, mode, hour, hour + timeSlice, days);
+
+				Time startWriting = Time.start.plusDays(lastDay).plusHours(hour + timeSlice);
+				actions.add(pair(startWriting, agg::writeMatrix));
+
+				for (DayOfWeek day : days) {
+					Time start = Time.start.plusDays(day.getTypeAsInt()).plusHours(hour);
+					Time stop = start.plusHours(timeSlice);
+
+					activations.add(pair(start, agg));
+					deactivations.add(pair(stop, agg));
+				}
+
+			}
+		}
+
+		return this;
+	}
+
+	/**
 	 * Returns a {@link SimpleEntry} containing the given key and value.
 	 *
 	 * @param <K>   the key type
@@ -193,6 +270,44 @@ public class ActiveListenersManagerBuilder {
 	 */
 	private static <K, V> SimpleEntry<K, V> pair(K key, V value) {
 		return new SimpleEntry<K, V>(key, value);
+	}
+
+	/**
+	 * Log the planned schedule. Log the activation, deactivation and action times.
+	 *
+	 * @return this {@link ActiveListenersManagerBuilder}
+	 */
+	public ActiveListenersManagerBuilder logPlannedSchedule() {
+		Time time = Time.start;
+		Time end = Time.start.plusDays(7);
+
+		while (time.isBeforeOrEqualTo(end)) {
+			final Time current = time;
+
+			long activate = this.activations
+				.stream()
+				.filter(e -> e.getKey().equals(current))
+				.count();
+			long deactivate = this.deactivations
+				.stream()
+				.filter(e -> e.getKey().equals(current))
+				.count();
+			long perform = this.actions.stream().filter(e -> e.getKey().equals(current)).count();
+
+			if (deactivate > 0) {
+				log.info("[{}] -> deactivate: {}", current.toString(), deactivate);
+			}
+			if (perform > 0) {
+				log.info("[{}] -> perform actions: {}", current.toString(), perform);
+			}
+			if (activate > 0) {
+				log.info("[{}] -> activate: {}", current.toString(), activate);
+			}
+
+			time = time.plusHours(1);
+		}
+
+		return this;
 	}
 
 }
--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/simulation/AggregateDemandFactory.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/simulation/AggregateDemandFactory.java
@@ -1,9 +1,11 @@
 package edu.kit.ifv.mobitopp.simulation;
 
 import java.io.File;
+import java.util.Collection;
 import java.util.List;
 import java.util.function.BiPredicate;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 import edu.kit.ifv.mobitopp.data.IntegerMatrix;
 import edu.kit.ifv.mobitopp.data.ZoneId;
@@ -27,8 +29,8 @@ public class AggregateDemandFactory {
 	private SimulationContext context;
 
 	/**
-	 * Instantiates a new {@link AggregateDemandFactory} with the given {@link MatrixPrinter} and
-	 * SimulationContext.
+	 * Instantiates a new {@link AggregateDemandFactory} with the given
+	 * {@link MatrixPrinter} and SimulationContext.
 	 *
 	 * @param context the context
 	 * @param printer the printer
@@ -39,8 +41,9 @@ public class AggregateDemandFactory {
 	}
 
 	/**
-	 * Instantiates a new {@link AggregateDemandFactory} with the given {@link SimulationContext}
-	 * and a {@link ZipMatrixPrinterDecorator} for printing matrices.
+	 * Instantiates a new {@link AggregateDemandFactory} with the given
+	 * {@link SimulationContext} and a {@link ZipMatrixPrinterDecorator} for
+	 * printing matrices.
 	 *
 	 * @param context the context
 	 */
@@ -60,33 +63,63 @@ public class AggregateDemandFactory {
 	 * @return the aggregate demand person listener
 	 */
 	public AggregateDemandOfStartedTrips newWeekdayListener(List<ZoneId> ids, Mode mode, int hour,
-			String suffix) {
+		String suffix) {
 
 		return new AggregateDemandOfStartedTrips(ids, filterBy(mode, hour).and(weekdayFilter()),
-				createMatrixWriter(mode, hour, hour+1, suffix), scaleFactor());
+			createMatrixWriter(mode, hour, hour + 1, suffix), scaleFactor());
 	}
 
 	/**
 	 * Creates a new day listener which will listen for results on a specific day
 	 * during the given hour, for the given {@link Mode} and zoneIds.
 	 *
-	 * @param ids       the {@link ZoneId zone ids} for which demand will be aggregated
+	 * @param ids       the {@link ZoneId zone ids} for which demand will be
+	 *                  aggregated
 	 * @param mode      the {@link Mode mode} for which demand will be aggregated
 	 * @param hour      the hour during which demand will be aggregated
-	 * @param dayOfWeek the {@link DayOfWeek day of week} on which demand will be aggregated
+	 * @param dayOfWeek the {@link DayOfWeek day of week} on which demand will be
+	 *                  aggregated
 	 * @return the aggregate demand person listener
 	 */
 	public AggregateDemandOfStartedTrips newDayListener(List<ZoneId> ids, Mode mode, int hour,
-			DayOfWeek dayOfWeek) {
+		DayOfWeek dayOfWeek) {
 
 		return new AggregateDemandOfStartedTrips(ids,
-				filterBy(mode, hour).and(dayOfWeekFilter(dayOfWeek)),
-				createMatrixWriter(mode, hour, hour+1, dayOfWeek.name().toLowerCase()),
-				scaleFactor());
+			filterBy(mode, hour).and(dayOfWeekFilter(dayOfWeek)),
+			createMatrixWriter(mode, hour, hour + 1, dayOfWeek.name().toLowerCase()),
+			scaleFactor());
 	}
 
 	/**
-	 * Create a BiPredicate to filter {@link StartedTrip started trips} by {@link Mode} and hour.
+	 * Creates a new listener which will listen for results in the given time slice
+	 * [timeFrom, timeTo] on the given set of days, for the given {@link Mode} and
+	 * zoneIds.
+	 *
+	 * @param ids       the {@link ZoneId zone ids} for which demand will be
+	 *                  aggregated
+	 * @param mode      the {@link Mode mode} for which demand will be aggregated
+	 * @param hourFrom the hour from which demand will be aggregated (inclusive)
+	 * @param hourTo   the hour until which demand will be aggregated (exclusive)
+	 * @param days     the {@link DayOfWeek days of week} on which demand will be
+	 *                  aggregated
+	 * @return the aggregate demand of started trips
+	 */
+	public AggregateDemandOfStartedTrips newListener(List<ZoneId> ids, Mode mode, int hourFrom,
+		int hourTo, Collection<DayOfWeek> days) {
+		String suffix = days
+			.stream()
+			.map(DayOfWeek::name)
+			.map(s -> s.substring(0, 2))
+			.collect(Collectors.joining("-"));
+
+		return new AggregateDemandOfStartedTrips(ids,
+			filterBy(mode, hourFrom, hourTo).and(dayOfWeekFilter(days)),
+			createMatrixWriter(mode, hourFrom, hourTo, suffix), scaleFactor());
+	}
+
+	/**
+	 * Create a BiPredicate to filter {@link StartedTrip started trips} by
+	 * {@link Mode} and hour.
 	 *
 	 * @param mode the {@link Mode mode}
 	 * @param hour the hour
@@ -94,6 +127,20 @@ public class AggregateDemandFactory {
 	 */
 	private BiPredicate<Person, StartedTrip<?>> filterBy(Mode mode, int hour) {
 		return modeFilter(mode).and(hourFilter(hour));
+	}
+
+	
+	/**
+	 * Create a BiPredicate to filter {@link StartedTrip started trips} by
+	 * {@link Mode} and time slice [hourFrom, hourTo].
+	 *
+	 * @param mode the {@link Mode mode}
+	 * @param hourFrom lower bound of the time slice (inclusive)
+	 * @param hourTo upper bound of the time slice (exclusive)
+	 * @return the bi predicate
+	 */
+	private BiPredicate<Person, StartedTrip<?>> filterBy(Mode mode, int hourFrom, int hourTo) {
+		return modeFilter(mode).and(hourFilter(hourFrom, hourTo));
 	}
 
 	/**
@@ -114,7 +161,8 @@ public class AggregateDemandFactory {
 		File toOutputFile = new File(matrixFolder, fileName);
 
 		return matrix -> {
-			printer.writeMatrixToFile(matrix, Integer.toString(from), Integer.toString(to),
+			printer
+				.writeMatrixToFile(matrix, Integer.toString(from), Integer.toString(to),
 					toOutputFile);
 		};
 	}
@@ -142,6 +190,18 @@ public class AggregateDemandFactory {
 	}
 
 	/**
+	 * Creates a Day-of-week filter for filtering trips taking place on one of the given
+	 * days.
+	 *
+	 * @param dayTypes the {@link DayOfWeek days of the week} to be filtered
+	 * @return the filter
+	 */
+	public static BiPredicate<Person, StartedTrip<?>> dayOfWeekFilter(
+		Collection<DayOfWeek> dayTypes) {
+		return (person, trip) -> dayTypes.contains(trip.startDate().weekDay());
+	}
+
+	/**
 	 * Creates a weekend filter for filtering trips taking place on the weekend.
 	 *
 	 * @return the filter
@@ -157,9 +217,11 @@ public class AggregateDemandFactory {
 	 * @return the filter
 	 */
 	public static BiPredicate<Person, StartedTrip<?>> weekdayFilter() {
-		return dayOfWeekFilter(DayOfWeek.MONDAY).or(dayOfWeekFilter(DayOfWeek.TUESDAY))
-				.or(dayOfWeekFilter(DayOfWeek.WEDNESDAY)).or(dayOfWeekFilter(DayOfWeek.THURSDAY))
-				.or(dayOfWeekFilter(DayOfWeek.FRIDAY));
+		return dayOfWeekFilter(DayOfWeek.MONDAY)
+			.or(dayOfWeekFilter(DayOfWeek.TUESDAY))
+			.or(dayOfWeekFilter(DayOfWeek.WEDNESDAY))
+			.or(dayOfWeekFilter(DayOfWeek.THURSDAY))
+			.or(dayOfWeekFilter(DayOfWeek.FRIDAY));
 	}
 
 	/**
@@ -170,6 +232,20 @@ public class AggregateDemandFactory {
 	 */
 	public static BiPredicate<Person, StartedTrip<?>> hourFilter(int hour) {
 		return (person, trip) -> trip.startDate().getHour() == hour;
+	}
+
+	/**
+	 * Creates an hour filter for filtering trips taking place in the give time slice [hourFrom, hourTo].
+	 *
+	 * @param hourFrom the lower bound of the time slice (inclusive)
+	 * @param hourTo the upper bound of the time slice (exclusive)
+	 * @return the filter
+	 */
+	public static BiPredicate<Person, StartedTrip<?>> hourFilter(int hourFrom, int hourTo) {
+		return (person, trip) -> {
+			int hour = trip.startDate().getHour();
+			return hourFrom <= hour && hour < hourTo;
+		};
 	}
 
 	/**
