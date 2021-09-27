e044b41dc869285c6600db05d14ac2a652f727a4
Lars Briem <lars.briem@kit.edu>
2021-09-27 19:53:41 +0200

    #439 Read compressed timetable files
    Allow timetable files to be stored in compressed files.

--- mobitopp/src/main/java/edu/kit/ifv/mobitopp/publictransport/serializer/BaseDeserializer.java
+++ mobitopp/src/main/java/edu/kit/ifv/mobitopp/publictransport/serializer/BaseDeserializer.java
@@ -1,10 +1,12 @@
 package edu.kit.ifv.mobitopp.publictransport.serializer;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
+import java.io.InputStreamReader;
 import java.util.stream.Stream;
+
+import edu.kit.ifv.mobitopp.util.file.StreamContent;
 
 abstract class BaseDeserializer {
 
@@ -15,7 +17,7 @@ abstract class BaseDeserializer {
 	}
 
 	protected Stream<String> removeHeaderFrom(File file) throws IOException {
-		return Files.lines(file.toPath(), StandardCharsets.UTF_8).skip(header);
+		return new BufferedReader(new InputStreamReader(StreamContent.of(file))).lines().skip(header);
 	}
 
 }
