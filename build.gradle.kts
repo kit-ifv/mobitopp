408710cbe139b4425a256b9731bc66e41acdcbc0
Robin Andre <fi7528@kit.edu>
2023-04-12 17:03:34 +0200

    trying to integrate the print by using another plugin from stackoverflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.8.0"
     jacoco
+    id("org.barfuin.gradle.jacocolog") version "1.2.4"
     application
 }
 
@@ -25,10 +26,6 @@ tasks.jacocoTestReport {
     reports {
         xml.required.set(true)
         csv.required.set(false)
-        println(layout)
-        println(layout.buildDirectory)
-
-        println(outputs.toString())
     }
 
 
