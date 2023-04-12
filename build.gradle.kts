bc8ad38067a8f77352d77f825f2e2a2134a04a0a
robin.andre <robin.andre@kit.edu>
2023-04-12 16:00:21 +0200

    Integrating Testing and Coverage into CICD

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+    jacoco
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        csv.required.set(false)
+        println(layout)
+        println(layout.buildDirectory)
+
+    }
+}
+
+
+kotlin {
+    jvmToolchain(11)
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ src/test/kotlin/ExampleTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+class ExampleTest {
+    @Test
+    fun creatorTest() {
+        assertEquals("Robin", creator())
+    }
+
+}
