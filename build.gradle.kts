9fde9c68e1f934a7e104f1a9b43a10c5eeb24ceb
Robin Andre <fi7528@kit.edu>
2023-04-14 10:46:07 +0200

    kotlin code style taken from https://medium.com/@danielvivek/code-styling-for-kotlin-fa7a8291c254

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm") version "1.8.0"
     jacoco
     id("org.barfuin.gradle.jacocolog") version "1.2.4" //This plugin is necessary because gradle eats the console output and gitlab demands to parse the console output for a coverage badge
+    id("io.gitlab.arturbosch.detekt") version "1.0.0.RC6-4"
     application
 }
 
@@ -12,6 +13,13 @@ repositories {
     mavenCentral()
 }
 
+detekt {
+    defaultProfile {
+        input = "$projectDir/subproject/src/main/kotlin"
+        config = "$projectDir/default-detekt-config.yml" // Code style rules file.
+        filters = ".*/res/.*,.*build/.*"
+    }
+}
 dependencies {
     testImplementation(kotlin("test"))
 }
