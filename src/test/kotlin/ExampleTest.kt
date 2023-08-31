7c69310c07f909e92442195b9ec748227d6aa64a
Robin Andre <fi7528@kit.edu>
2023-08-31 11:37:21 +0200

    made a failing test to see if the merge request is denied

--- src/test/kotlin/ExampleTest.kt
+++ src/test/kotlin/ExampleTest.kt
@@ -5,11 +5,11 @@ import kotlin.test.assertNotEquals
 class ExampleTest {
     @Test
     fun creatorTest() {
-        assertEquals("Robin", creator())
+        assertEquals("Robiin", creator())
     }
+
     @Test
     fun nonCreatorTest() {
         assertNotEquals("PETER", creator())
     }
-
 }
