7c66234809a1a1fb5cebac51377ede7144f26072
Robin Andre <fi7528@kit.edu>
2023-10-24 12:16:55 +0200

    collections are now passed as val mutableX instead of var

--- codegen/processor/src/main/kotlin/Processor.kt
+++ codegen/processor/src/main/kotlin/Processor.kt
@@ -1,5 +1,4 @@
-import com.google.devtools.ksp.getDeclaredProperties
-import com.google.devtools.ksp.getVisibility
+import com.google.devtools.ksp.isAbstract
 import com.google.devtools.ksp.processing.*
 import com.google.devtools.ksp.symbol.*
 import com.google.devtools.ksp.validate
@@ -7,7 +6,24 @@ import java.io.OutputStream
 
 
 
-
+fun properType(property: KSPropertyDeclaration): String {
+    val prefix = property.simpleName.asString()
+    val text = property.type.toString()
+    val collection = listOf("List", "Map", "Set", "Collection").contains(text)
+    val resolved = property.type.resolve()
+    var generics = ""
+    if(resolved.arguments.isNotEmpty()) {
+        generics += resolved.arguments.joinToString(
+            separator = ", ",
+            prefix = "<",
+            postfix = ">"
+        ) { it.type.toString() }
+    }
+    val announce = if (collection)  "val" else "var"
+    val pronounceMutability = if(collection) "Mutable" else ""
+    val default = if (collection) " = mutable${text}Of()" else ""
+    return "$announce $prefix : $pronounceMutability$text$generics"
+}
 
 
 class Processor(private val codeGenerator: CodeGenerator, private val logger: KSPLogger, private val options: Map<String, String>) : SymbolProcessor {
@@ -20,25 +36,47 @@ class Processor(private val codeGenerator: CodeGenerator, private val logger: KS
 
         val symbols = resolver.getSymbolsWithAnnotation("Mutable").filterIsInstance<KSClassDeclaration>()
         if (!symbols.iterator().hasNext()) return emptyList()
+        val symbolList = symbols.toList()
 
-        val file = codeGenerator.createNewFile(
-            dependencies = Dependencies(false, *resolver.getAllFiles().toList().toTypedArray()),
-            packageName = "changeLater",
-            fileName = "TestFile"
-        )
-        file += "package data\n"
+        val packageMap = symbolList.groupBy { it.packageName }
+        packageMap.forEach {
+            val packageName = it.key.asString()
+            val file = codeGenerator.createNewFile(
+                dependencies = Dependencies(false, *resolver.getAllFiles().toList().toTypedArray()),
+                packageName = packageName,
+                fileName = "Builders"
+            )
+            if (packageName.isNotEmpty()) {
+                file += "package $packageName\n"
+            }
 
-        symbols.forEach { it.accept(Visitor(file), Unit) }
-
-        file.close()
+            it.value.forEach{x -> x.accept(Visitor(file), Unit)}
+            file.close()
+        }
+//        val file = codeGenerator.createNewFile(
+//            dependencies = Dependencies(false, *resolver.getAllFiles().toList().toTypedArray()),
+//            packageName = "changeLater",
+//            fileName = "Builders"
+//        )
+//        file += "package data\n"
+//
+//        symbols.forEach { it.accept(Visitor(file), Unit) }
+//
+//        file.close()
         return symbols.filterNot { it.validate() }.toList()
     }
     inner class Visitor(private val file: OutputStream): KSVisitorVoid() {
         override fun visitClassDeclaration(classDeclaration: KSClassDeclaration, data: Unit) {
+            // Currently this disables mutable annotation for abstract classes and interfaces
+            if(classDeclaration.isAbstract()) {
+                return
+            }
             val className = classDeclaration.simpleName.asString()
             file += "class Mutable$className (\n"
-            file += classDeclaration.getAllProperties().map {"var " +
-                    it.simpleName.asString() + ": " + it.type.toString()}.joinToString(prefix = "    ", postfix = "\n", separator = ",\n    ")
+            classDeclaration.getAllProperties().map { it.accept(this, Unit) }
+            file += classDeclaration.getAllProperties().map {
+                properType(it)
+            }.joinToString(prefix = "    ", postfix = "\n", separator = ",\n    ")
 
             file += ") {\n"
 
@@ -53,7 +91,6 @@ class Processor(private val codeGenerator: CodeGenerator, private val logger: KS
         }
 
         override fun visitPropertyDeclaration(property: KSPropertyDeclaration, data: Unit) {
-
         }
 
         override fun visitTypeArgument(typeArgument: KSTypeArgument, data: Unit) {
