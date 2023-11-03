88983108ecb794169fcaf9d728c451ccc6ef413e
Robin Andre <fi7528@kit.edu>
2023-11-03 15:10:03 +0100

    added coding scheme for integer to object conversions

---
+++ src/main/kotlin/data/Coding.kt
@@ -0,0 +1,21 @@
+package data
+
+/**
+ * An object is encodable if it can provide an integer based on the attributes present. In the future this could be
+ * abstracted to a template type in case that the encoding might return a different type (such as Strings)
+ */
+fun interface Encodable {
+    fun encode(): Int
+}
+
+/**
+ * Similarly to an encoding a decoding interface allows to construct the underlying type by the provided integer code.
+ * As the decoding logic can not be realistically dependent on an explicit object instantiation the logic must be bound
+ * to a different object and cannot be added to a shared interface providing both encoding and decoding. This turns out
+ * to be beneficial as both interfaces can be functional.
+ *
+ * This could also be abstracted to generic decoding types if the need arises
+ */
+fun interface Decodable<T: Encodable> {
+    fun decode(i: Int): T
+}
