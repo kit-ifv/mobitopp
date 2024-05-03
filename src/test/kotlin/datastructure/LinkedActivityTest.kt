f95d871e16563f5478722e83828985980b3886e4
Robin Andre <fi7528@kit.edu>
2024-05-03 15:40:58 +0200

    added test factory for linked actions

--- src/test/kotlin/datastructure/LinkedActivityTest.kt
+++ src/test/kotlin/datastructure/LinkedActivityTest.kt
@@ -4,6 +4,8 @@ import FOURTH
 import OTHER
 import START
 import THIRD
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
 import org.junit.jupiter.api.assertThrows
 import kotlin.test.BeforeTest
 import kotlin.test.Test
@@ -25,23 +27,38 @@ class LinkedActivityTest {
         thirdActivity =
             LinkedActivity(Activity.fromDuration(OTHER, 3.hours, 1.hours), { secondLeg }, { fourthActivity })
         fourthActivity = LinkedActivity(Activity.fromDuration(OTHER, 5.hours, 1.hours), { thirdActivity }, { fifthLeg })
-        fifthLeg = LinkedLeg(Leg.fromDuration(7.hours, 1.hours, START, OTHER), { fourthActivity }, { null })
+        fifthLeg = LinkedLeg(Leg.fromDuration(7.hours, 1.hours, OTHER, START), { fourthActivity }, { null })
     }
+    @TestFactory
+    fun changingValues(): List<DynamicTest> {
+        val actions: List<(Location) -> Unit> = listOf(
+            { secondLeg.endLocation = it },
 
-    @Test
-    fun changingValues() {
-        firstLeg.endLocation = FOURTH
-        assertEquals(firstLeg.endLocation, FOURTH)
-        assertEquals(secondLeg.startLocation, FOURTH)
-    }
+            { thirdActivity.startLocation = it },
+            { thirdActivity.location = it },
+            { thirdActivity.endLocation = it },
 
-    @Test
-    fun changingActivityLocation() {
-        thirdActivity.location = FOURTH
-        assertEquals(thirdActivity.location, FOURTH)
-        assertEquals(fourthActivity.location, FOURTH)
-        assertEquals(fifthLeg.startLocation, FOURTH)
-        assertEquals(secondLeg.endLocation, FOURTH)
+            { fourthActivity.startLocation = it },
+            { fourthActivity.location = it },
+            { fourthActivity.endLocation = it },
+
+            { fifthLeg.startLocation = it },
+        )
+        return actions.map {
+            DynamicTest.dynamicTest(it.toString()) {
+                // Weirdly, neither @BeforeEach nor @BeforeTest actually call setup() from the test factory.
+                setup()
+                assertEquals(thirdActivity.location, OTHER)
+                assertEquals(fourthActivity.location, OTHER)
+                assertEquals(fifthLeg.startLocation, OTHER)
+                assertEquals(secondLeg.endLocation, OTHER)
+                it(FOURTH)
+                assertEquals(thirdActivity.location, FOURTH)
+                assertEquals(fourthActivity.location, FOURTH)
+                assertEquals(fifthLeg.startLocation, FOURTH)
+                assertEquals(secondLeg.endLocation, FOURTH)
+            }
+        }
     }
 
     @Test
@@ -49,5 +66,38 @@ class LinkedActivityTest {
         assertThrows<IllegalStateException> { secondLeg.startTime = 0.5.hours }
         assertEquals(firstLeg.endTime, 1.hours)
         assertEquals(secondLeg.startTime, 1.hours)
+    }
+
+    @Test
+    fun badChanges2() {
+        assertThrows<IllegalStateException> { secondLeg.endTime = 3.5.hours }
+        assertEquals(thirdActivity.startTime, 3.hours)
+        assertEquals(secondLeg.endTime, 2.hours)
+    }
+
+    @Test
+    fun badChanges3() {
+        assertThrows<IllegalStateException> { thirdActivity.startTime = 1.5.hours }
+        assertEquals(thirdActivity.startTime, 3.hours)
+        assertEquals(secondLeg.endTime, 2.hours)
+    }
+
+    @Test
+    fun badChanges4() {
+        assertThrows<IllegalStateException> { thirdActivity.endTime = 5.5.hours }
+        assertEquals(thirdActivity.endTime, 4.hours)
+        assertEquals(fourthActivity.startTime, 5.hours)
+    }
+
+    @Test
+    fun validChange() {
+        secondLeg.endTime = 2.5.hours
+        assertEquals(secondLeg.endTime, 2.5.hours)
+        assertEquals(thirdActivity.startTime, 3.hours)
+    }
+
+    @Test
+    fun badChanges5() {
+        assertThrows<IllegalStateException> { secondLeg.endTime = 0.9.hours }
     }
 }
