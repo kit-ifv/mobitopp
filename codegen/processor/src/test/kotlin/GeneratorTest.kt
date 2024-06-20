bf5907fd3b22e3e05cd09f2963bf3b9787b5efec
Robin Andre <fi7528@kit.edu>
2024-06-20 11:55:47 +0200

    fixed detekt

--- codegen/processor/src/test/kotlin/GeneratorTest.kt
+++ codegen/processor/src/test/kotlin/GeneratorTest.kt
@@ -1,24 +1,22 @@
 
 import com.tschuchort.compiletesting.KotlinCompilation
 import com.tschuchort.compiletesting.SourceFile
-import com.tschuchort.compiletesting.kspSourcesDir
 import com.tschuchort.compiletesting.kspWithCompilation
 import com.tschuchort.compiletesting.symbolProcessorProviders
 import org.junit.Test
-import java.io.File
 import kotlin.test.assertEquals
 
-class CompileTest {
+class GeneratorTest {
 
     @Test
     fun `test simple compilation`() {
         val source = SourceFile.kotlin("Hello.kt", TEST1)
-        lateinit var output: File
+//        lateinit var output: File
         val result = KotlinCompilation().apply {
             sources = listOf(source)
             symbolProcessorProviders = listOf(ProcessorProvider())
             inheritClassPath = true
-            output = kspSourcesDir
+//            output = kspSourcesDir
             messageOutputStream = System.out // See compiler messages in the console
             kspWithCompilation = true
         }.compile()
