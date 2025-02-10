44d32490e5c11414212eed5a5d21b8e661d777b5
ar0305 <jelle.kuebler@kit.edu>
2025-02-10 11:12:26 +0100

    added some comments/todos while reviewing data model

--- src/main/kotlin/domain/data/Zone.kt
+++ src/main/kotlin/domain/data/Zone.kt
@@ -12,6 +12,27 @@ import utils.random.SeededActor
 
 typealias ZoneId = ID<Zone>
 
+/**
+ * Zone - a traffic assignment zone in a transport model.
+ *
+ * Zones have:
+ * - a [visumId] to reference the base visum model
+ * - a [name] as description (mostly for debugging and logging)
+ * - //TODO naming of these properties might be confusing
+ * - an [areaType] as classification of the zones regional function/characteristics
+ * - a [regionType] Integer code
+ * - a survey area [classification]
+ * - a number of public [parkingPlaces]
+ * - a flag to denote whether it [isDestination] in destination choice
+ * - a [relief] - the standard deviation of elevation in the zone
+ * - a [centroid] location
+ *
+ * @property id
+ * @constructor
+ *
+ * @param centroid
+ * @param seed
+ */
 @Mutable
 abstract class Zone(
     override val id: ZoneId,
@@ -19,7 +40,7 @@ abstract class Zone(
     seed: Long,
 ) : SeededActor<Zone>(seed), Identifiable<ZoneId> {
 
-    abstract val visumId: Long
+    abstract val visumId: Long //TODO not a general property of zone, only here because we use visum
     abstract val name: String
     abstract val areaType: AreaType
     abstract val regionType: Int
