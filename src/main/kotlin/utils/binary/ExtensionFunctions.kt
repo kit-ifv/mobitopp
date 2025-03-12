9e35954220be30ac583c41e1f566c13d1f24e2e1
Robin Andre <fi7528@kit.edu>
2025-03-12 15:57:12 +0100

    added some documentation to binary conversion primary calls, and made RandomAccessFile() close by using .use

---
+++ src/main/kotlin/utils/binary/ExtensionFunctions.kt
@@ -0,0 +1,61 @@
+package utils.binary
+
+import domain.location.Location
+import java.io.DataOutputStream
+import java.io.RandomAccessFile
+import java.nio.MappedByteBuffer
+import java.nio.channels.FileChannel
+import java.nio.file.Path
+
+fun <T> Path.operateOnMemoryFile(run : MappedByteBuffer.() -> T): T {
+    return RandomAccessFile(toFile(), "r").use { file ->
+        val channel = file.channel
+        val size = channel.size()
+        val buffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, size)
+        buffer.run(run)
+    }
+}
+
+/**
+ * Extension function for `DataOutputStream` that writes a `Location` object to the output stream.
+ * The method serializes the properties of the `Location` object (zone, coordinate, and road access)
+ * into the output stream in a specific format:
+ * - The `zone.id` is written as a `Long` (or `Long.MIN_VALUE` if `zone.id` is `null`).
+ * - The latitude and longitude of the `coordinate` are written as `Double` values.
+ * - The `roadAccess.roadId` is written as a `Long` (or `Long.MIN_VALUE` if `roadAccess.roadId` is `null`).
+ * - The position of the `roadAccess` is written as a `Double` (with a default value of `0.5` if `roadAccess.position` is `null`).
+ *
+ * @param location The `Location` object to write to the `DataOutputStream`.
+ */
+fun DataOutputStream.writeLocation(location: Location) {
+    LocationUtils.writeLocation(location, this)
+}
+
+/**
+ * Extension function for `MappedByteBuffer` that retrieves a boolean value at a specified index.
+ *
+ * This function interprets the integer stored at the given `index` as a bit mask and extracts the boolean
+ * value based on the specific bit position (the first byte of the integer). The integer is read from the buffer
+ * and bitwise AND-ed with a mask (0x01000000) to determine whether the corresponding boolean flag is set.
+ *
+ * The reasoning behind using an integer and bitwise masking is that `MappedByteBuffer` does not have a convenient
+ * way to directly read a boolean, and the boolean value is often stored as part of a larger integer structure.
+ * The mask checks whether the first byte (0x01) in the integer block is set, which indicates a `true` value.
+ * If it's not set, the boolean is considered `false`.
+ *
+ * **Note:** The mask (0x01000000) specifically checks the first byte in the 4-byte integer, which is assumed
+ * to store the boolean flag (1 byte). This method is useful when working with binary data or file formats where
+ * a boolean flag is embedded in an integer value.
+ *
+ * @param index The position (index) in the [MappedByteBuffer] where the integer representing the boolean is stored.
+ * @return The boolean value extracted from the 4-byte integer at the specified index. Returns `true` if the first byte is set (0x01), otherwise `false`.
+ */
+fun MappedByteBuffer.getBoolean(index: Int): Boolean {
+    val b = getInt(index)
+    /* This magic number is the hexadecimal representation of an integer block in memory, such as 01 XX XX XX
+    since the boolean flag is written as a byte of 01, the quickest way to check whether the integer at that location
+    matches this mask 01-XX-XX-XX, because there is no convenient way to get a boolean from a bytebuffer, and no
+    convenient way to apply mask operations on anything but an int.
+    * */
+    return (b and 0x01000000) != 0
+}
--- src/test/kotlin/utils/binary/BinaryContextTest.kt
+++ src/test/kotlin/utils/binary/BinaryContextTest.kt
@@ -5,6 +5,7 @@ import domain.enums.Regiostar17
 import modeling.steps.Identifiable
 import modeling.steps.Repository
 import modeling.steps.Run
+import org.junit.jupiter.api.BeforeAll
 import usecases.LegacyMode
 import usecases.steps.ProjectContext
 import usecases.steps.legacyData.loadHouseholds
@@ -13,6 +14,7 @@ import usecases.steps.legacyData.loadZones
 import usecases.steps.loadActivities
 import usecases.steps.loadPersons
 import java.io.File
+import java.nio.file.Paths
 import kotlin.io.path.Path
 import kotlin.test.Test
 import kotlin.test.assertContentEquals
@@ -23,52 +25,9 @@ private val ROOT_FS = "\\\\ifv-fs\\Forschung\\Projekte_intern\\mobitopp\\Output"
 private val rootRastatt = File("$ROOT_FS\\logiktram_rastatt_long-term-module\\rastatt")
 
 class BinaryContextTest {
-    @Test
-    fun householdTest() {
-        Run {
-            ProjectContext(
-                scenarioName = "testSteps",
-                areaTypeCodes = Regiostar17,
-                demandFolder = rootRastatt,
-                economicalStatusCodes = EconomicStatus,
-                simulationSeed = 42,
-                modes = LegacyMode,
-            )
-        }.steps {
-            loadZones()
-            loadHouseholds()
-            loadPersons()
-            loadPrivateCars()
-            loadActivities()
-            writeZonesBinary(Path("src/test/resources/binaryZone.bin"))
-            writeHouseholdBinary(Path("src/test/resources/binaryHousehold.bin"))
-            writePersonsBinary(Path("src/test/resources/binaryPerson.bin"))
-            writeActivitiesBinary(Path("src/test/resources/binaryPlannedActivities.bin"))
-            writeCarsBinary(Path("src/test/resources/binaryCars.bin"))
-        }
-    }
 
     @Test
     fun readBinaryTest() {
-//        Run {
-//            ProjectContext(
-//                scenarioName = "testSteps",
-//                areaTypeCodes = Regiostar17,
-//                demandFolder = rootRastatt,
-//                economicalStatusCodes = EconomicStatus,
-//                simulationSeed = 42,
-//                modes = LegacyMode,
-//            )
-//        }.steps {
-//            loadZonesFromBinary(Path("src/test/resources/binaryZone.bin"))
-//            loadHouseholdFromBinary(Path("src/test/resources/binaryHousehold.bin"))
-//            loadPersonsFromBinary(Path("src/test/resources/binaryPerson.bin"))
-//            loadActivitiesFromBinary(Path("src/test/resources/binaryPlannedActivities.bin"))
-//            loadCarsFromBinary(Path("src/test/resources/binaryCars.bin"))
-//
-//            println("Wololo")
-//        }
-
         val x = ProjectContext(
             scenarioName = "testSteps",
             areaTypeCodes = Regiostar17,
@@ -105,10 +64,40 @@ class BinaryContextTest {
     private fun <E : utils.Identifiable<I>, I> repoEquals(x: Repository<E, I>, y: Repository<E, I>) {
         val expected = x.elements.toList()
         val actual = y.elements.toList()
-        val a = expected[0]
-        val b = actual[0]
-        val equals = a == b
         assertContentEquals(expected, actual)
+    }
+
+    companion object {
+        @JvmStatic
+        @BeforeAll
+        fun setup(): Unit {
+            /*TODO find a way to only create these files when necessary, also the pipeline may be unhappy about this
+               test because the paths may not be visible to the gitlab runner */
+            createBinaryFiles()
+        }
+        private fun createBinaryFiles() {
+            Run {
+                ProjectContext(
+                    scenarioName = "testSteps",
+                    areaTypeCodes = Regiostar17,
+                    demandFolder = rootRastatt,
+                    economicalStatusCodes = EconomicStatus,
+                    simulationSeed = 42,
+                    modes = LegacyMode,
+                )
+            }.steps {
+                loadZones()
+                loadHouseholds()
+                loadPersons()
+                loadPrivateCars()
+                loadActivities()
+                writeZonesBinary(Path("src/test/resources/binaryZone.bin"))
+                writeHouseholdBinary(Path("src/test/resources/binaryHousehold.bin"))
+                writePersonsBinary(Path("src/test/resources/binaryPerson.bin"))
+                writeActivitiesBinary(Path("src/test/resources/binaryPlannedActivities.bin"))
+                writeCarsBinary(Path("src/test/resources/binaryCars.bin"))
+            }
+        }
     }
 
 }
