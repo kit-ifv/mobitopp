aec426a9110ea201c3de6d12f8b20247daacf736
ar0305 <ar0305@kit.edu>
2025-03-26 10:31:03 +0100

    Add model step to randomize activity duration, another option ti shift all activity starts by random offset is added to load planned activities

--- src/main/kotlin/usecases/steps/AssignPlannedActivities.kt
+++ src/main/kotlin/usecases/steps/AssignPlannedActivities.kt
@@ -1,5 +1,7 @@
 package usecases.steps
 
+import datastructure.Activity
+import datastructure.Schedule
 import datastructure.plans.SingularDispatcher
 import domain.data.ActivityId
 import domain.data.MutablePerson
@@ -16,6 +18,8 @@ import utils.random.getGaussian
 import kotlin.math.max
 import kotlin.math.min
 import kotlin.math.roundToInt
+import kotlin.random.Random
+import kotlin.time.Duration
 import kotlin.time.Duration.Companion.minutes
 
 fun AssignPlannedActivitiesContext.assignPlannedActivities() = runStep {
@@ -47,15 +51,6 @@ class AssignPlannedActivities(
         element.schedule = requireNotNull(activitiesPerPerson[element.id]) {
             "No activities found for Person ${element.id}" // TODO error handling here
         }.toSchedule(SingularDispatcher())
-
-        val schedule = element.schedule
-        val rand = element.random
-        for (i in 0 until schedule.activities().size) {
-            val act = schedule.activities().toList()[i]
-            val durMin = act.duration.inWholeMinutes
-            val deviation = (rand.getGaussian(0.0, 1.0) * durMin / 20.0).roundToInt()
-            act.duration = min(max(1.0, (durMin + deviation).toDouble()), 10080.0).minutes //TODO legacy shift algorithm is fixed to one week?!
-        }
     }
 
     override fun execute() = super.execute().also {
@@ -63,4 +58,53 @@ class AssignPlannedActivities(
         activitiesPerPerson.clear()
         System.gc()
     }
+}
+
+fun AssignPlannedActivitiesContext.randomizeActivityDurations(
+    activityDurationRandomizer: ActivityDurationRandomizer = GaussianActivityDurationRandomizer()
+) = runStep {
+    RandomizeActivityDuration(activityDurationRandomizer, this)
+}
+
+class RandomizeActivityDuration(
+    private val activityDurationRandomizer: ActivityDurationRandomizer = GaussianActivityDurationRandomizer(),
+    context: AssignPlannedActivitiesContext,
+) : UpdateEachStep<MutablePerson, PersonId>() {
+
+    override val name = "Randomize activity durations using ${activityDurationRandomizer::class.simpleName}"
+    override val repository = context.personRepository
+    override val dependentRepositories = setOf(context.plannedActivityRepository)
+
+    override fun update(element: MutablePerson) {
+        activityDurationRandomizer.randomizeAll(element.schedule, element.random)
+    }
+
+    override fun verifyInput(): Warning? = validate {
+        // TODO validation
+    }
+}
+
+abstract class ActivityDurationRandomizer {
+    fun randomizeAll(schedule: Schedule, rand: Random) {
+        for (i in 0 until schedule.activities().size) {
+            val act = schedule.activities().toList()[i]
+            act.duration = randomizeDuration(act, act.duration, rand)
+        }
+    }
+
+    abstract fun randomizeDuration(activity: Activity, currentDuration: Duration, rand: Random): Duration
+}
+
+@Suppress("MagicNumber")
+class GaussianActivityDurationRandomizer : ActivityDurationRandomizer() {
+    override fun randomizeDuration(activity: Activity, currentDuration: Duration, rand: Random): Duration {
+        val durMin = currentDuration.inWholeMinutes
+        val deviation = (rand.getGaussian(0.0, 1.0) * durMin / 20.0).roundToInt()
+        return min(max(1.0, (durMin + deviation).toDouble()), 10080.0).minutes
+        // TODO legacy shift algorithm is fixed to one week?!
+    }
+}
+
+object NoDurationRandomizer : ActivityDurationRandomizer() {
+    override fun randomizeDuration(activity: Activity, currentDuration: Duration, rand: Random) = currentDuration
 }
