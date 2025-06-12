0789bc58e600d3284d10c5703dd1cf3efbb2f22a
ar0305 <ar0305@kit.edu>
2025-06-12 11:21:40 +0200

    fixed stack overflow recursion error in BuildAgents
    toAgent was called before mutable parent agent was put into the lookup map. circular dependencies led to infinite recursion.
    
    added new Map extension function: getOrInitAfterPut which either returns the agent associated to the key or adds the new mutable agent to the map and then executes an init scope function to getOrCreate related child agents

--- src/main/kotlin/domain/simulation/agent/BuildAgents.kt
+++ src/main/kotlin/domain/simulation/agent/BuildAgents.kt
@@ -1,6 +1,7 @@
 package domain.simulation.agent
 
 import domain.shared.datastructure.schedule.plans.SingularDispatcher
+import domain.simulation.agent.location
 import domain.simulation.behavior.ActivityDurationRandomizer
 import domain.simulation.behavior.NoDurationRandomizer
 import domain.simulation.behavior.toSchedule
@@ -52,70 +53,98 @@ class BuildAgents(
     }
 }
 
-fun Household.toAgent(context: BuildAgents) = context.householdsById.getOrPut(this.id) {
-    val data = this
-    MutableHouseholdAgent(id, context.seed) {
-        this.householdNumber = data.householdNumber
-        this.surveyYear = data.surveyYear
-        this.location = data.location
-        this.domCode = data.domCode
-        this.type = data.type
-        this.incomePerMonth = data.incomePerMonth
-        this.economicStatus = data.economicStatus
-        this.members.addAll(
-            data.members.map { it.toAgent(context, this) }
-        )
-        this.cars.addAll(
-            data.cars.map { it.toAgent(context, this) }
-        )
+//To prevent recursion cycle, never call toAgent inside scope of getOrPut! Use initAfterPut scope instead
+private fun <K, V> MutableMap<K, V>.getOrInitAfterPut(
+    key: K,
+    defaultValue: () -> V,
+    initAfterPut: (V) -> Unit,
+): V {
+    var init = false
+    val result = this.getOrPut(key) {
+        init = true
+        defaultValue()
     }
+
+    if (init) {
+        initAfterPut(result)
+    }
+
+    return result
 }
 
+
+fun Household.toAgent(context: BuildAgents) = context.householdsById.getOrInitAfterPut(
+    key=this.id,
+    defaultValue = { MutableHouseholdAgent(id, context.seed) }
+) { agent ->
+    agent.householdNumber = this.householdNumber
+    agent.surveyYear = this.surveyYear
+    agent.location = this.location
+    agent.domCode = this.domCode
+    agent.type = this.type
+    agent.incomePerMonth = this.incomePerMonth
+    agent.economicStatus = this.economicStatus
+    agent.members.addAll(
+        this.members.map { it.toAgent(context, agent) }
+    )
+    agent.cars.addAll(
+        this.cars.map { it.toAgent(context, agent) }
+    )
+}
+
+
 fun Person.toAgent(context: BuildAgents, householdAgent: HouseholdAgent = household.toAgent(context)) =
-    context.personsById.getOrPut(this.id) {
-        val data = this
-        MutablePersonAgent(id, householdAgent, context.seed) {
-            this.age = data.age
-            this.employment = data.employment
-            this.sex = data.sex
-            this.graduation = data.graduation
-            this.income = data.income
-            this.hasBike = data.hasBike
-            this.hasCommuterTicket = data.hasCommuterTicket
-            this.hasLicense = data.hasLicense
-            this.eMobilityAcceptance = data.eMobilityAcceptance
-            this.chargingInfluence = data.chargingInfluence
-            this.sharingMemberships.addAll(
-                data.sharingMemberships.map { it.toAgent(context) }
-            )
+    context.personsById.getOrInitAfterPut(
+        key=this.id,
+        defaultValue =  { MutablePersonAgent(id, householdAgent, context.seed) }
+    ) { agent ->
 
-            this.schedule = data.plannedActivities.toSchedule(SingularDispatcher())
-            data.clearPlannedActivities() // clear to save memory
-            context.durationRandomizer.randomizeAll(this)
+        agent.age = this.age
+        agent.employment = this.employment
+        agent.sex = this.sex
+        agent.graduation = this.graduation
+        agent.income = this.income
+        agent.hasBike = this.hasBike
+        agent.hasCommuterTicket = this.hasCommuterTicket
+        agent.hasLicense = this.hasLicense
+        agent.eMobilityAcceptance = this.eMobilityAcceptance
+        agent.chargingInfluence = this.chargingInfluence
 
-            this.memberships.addAll(this.sharingMemberships)
-            this.memberships.add(this.household)
-        }
+        agent.sharingMemberships.addAll(
+            this.sharingMemberships.map { it.toAgent(context) }
+        )
+        agent.memberships.addAll(agent.sharingMemberships)
+        agent.memberships.add(agent.household)
+
+
+        agent.schedule = this.plannedActivities.toSchedule(SingularDispatcher())
+        this.clearPlannedActivities() // clear to save memory
+        context.durationRandomizer.randomizeAll(agent)
     }
+
 
 fun PrivateCar.toAgent(context: BuildAgents, ownerAgent: HouseholdAgent = owner.toAgent(context)) =
-    context.carsById.getOrPut(this.id) {
-        val data = this
-        MutablePrivateCarAgent(id, ownerAgent) {
-            this.segment = data.segment
-            this.seats = data.seats
-            this.engine = data.engine
-            this.mainUser = data.mainUser?.toAgent(context)
-        }
+    context.carsById.getOrInitAfterPut(
+        key=this.id,
+        defaultValue = { MutablePrivateCarAgent(id, ownerAgent) }
+    ) { agent ->
+
+        agent.segment = this.segment
+        agent.seats = this.seats
+        agent.engine = this.engine
+        agent.mainUser = this.mainUser?.toAgent(context)
+    //TODO idea to reduce copy: in Agent definition:
+    // pass long term entity for delegation of interface implementation,
+    // only overwrite parts where other agent types are now referenced!
     }
 
-fun SharingProvider.toAgent(context: BuildAgents) = context.sharingProvidersById.getOrPut(this.id) {
-    val data = this
-    MutableSharingProviderAgent(id, name, mode) {
-        this.stations.addAll(
-            data.stations.map { it.toAgent(context, this) }
-        )
-    }
+fun SharingProvider.toAgent(context: BuildAgents) = context.sharingProvidersById.getOrInitAfterPut(
+    key=this.id,
+    defaultValue = { MutableSharingProviderAgent(id, name, mode) }
+) { agent ->
+    agent.stations.addAll(
+        this.stations.map { it.toAgent(context, agent) }
+    )
 }
 
 private var vehicleIdCounter = 0L
