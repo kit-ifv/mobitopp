bf03a4ca2619df88aea553ec42403aeb8d5454d2
zq6966 <zq6966@partner.kit.edu>
2025-07-09 18:19:39 +0200

    made console log more visible

--- src/main/kotlin/utils/ReportBuilder/ReportBuilder.kt
+++ src/main/kotlin/utils/ReportBuilder/ReportBuilder.kt
@@ -139,6 +139,14 @@ class ReportBuilder(val reportTitle: String = "Run-Report") {
         val outputFile = outputDir.resolve("$reportTitle.html")
         outputFile.writeText(html)
         val absolutePath = "file://" + outputFile.absolutePathString()
-        println("Open report: " + absolutePath)
+        print("\n\n")
+        println( BOLD + BLUE + "OPEN RUN REPORT (crtl + rmb): " + absolutePath + RESET)
+        print("\n\n")
     }
 }
+
+const val RED = "\u001B[31m"
+const val GREEN = "\u001B[32m"
+const val BLUE = "\u001B[34m"
+const val BOLD = "\u001B[1m"
+const val RESET = "\u001B[0m"
