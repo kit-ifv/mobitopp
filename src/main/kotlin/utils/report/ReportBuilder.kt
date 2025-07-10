90af10b9e0b7a839583adbcf3c24d7be5c15e78a
zq6966 <zq6966@partner.kit.edu>
2025-07-10 17:24:30 +0200

    detekt + Added inline svg.

--- src/main/kotlin/utils/report/ReportBuilder.kt
+++ src/main/kotlin/utils/report/ReportBuilder.kt
@@ -1,25 +1,26 @@
-package utils.ReportBuilder
+package utils.report
 
+import kotlinx.html.AreaShape
+import kotlinx.html.Entities
+import kotlinx.html.SVG
 import kotlinx.html.body
 import kotlinx.html.div
 import kotlinx.html.h1
-import kotlinx.html.h3
 import kotlinx.html.h5
 import kotlinx.html.head
 import kotlinx.html.html
 import kotlinx.html.id
+import kotlinx.html.img
 import kotlinx.html.onClick
-import kotlinx.html.p
 import kotlinx.html.pre
 import kotlinx.html.script
 import kotlinx.html.span
 import kotlinx.html.stream.createHTML
 import kotlinx.html.style
+import kotlinx.html.svg
 import kotlinx.html.title
 import kotlinx.html.unsafe
-import org.jetbrains.kotlinx.dataframe.api.toPath
-import java.awt.Desktop
-import java.io.File
+import org.jetbrains.kotlinx.kandy.letsplot.style.LayoutParameters.Companion.line
 import java.nio.file.Path
 import kotlin.io.path.Path
 import kotlin.io.path.absolutePathString
@@ -34,7 +35,7 @@ private enum class ReportType(val cssClass: String) {
     WARNING("warning"),
     ERROR("error")
 }
-private abstract class Card (
+private abstract class Card(
     val name: String,
     val message: String,
     val type: ReportType
@@ -45,8 +46,16 @@ private abstract class Card (
         return createHTML().span {
             div("section " + type.cssClass) {
                 onClick = "toggle('$contentID')"
+                @Suppress("UnusedPrivateProperty")
                 val contentId = "section-content-$contentID"
-                span("toggle-button") {
+                span("toggle-button inline") {
+                    unsafe {
+                        +"""
+                        <svg width="10" height="6" viewBox="0 0 10 6" xmlns="http://www.w3.org/2000/svg" stroke="currentColor" fill="none" stroke-width="1.5">
+                          <path d="M1 1 L5 5 L9 1" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                        """.trimIndent()
+                    }
                     h5 {
                         +name
                     }
@@ -60,10 +69,10 @@ private abstract class Card (
     }
 }
 
-private class Warning(name: String, message: String): Card(name, message, ReportType.WARNING)
-private class Error(name: String, message: String): Card(name, message, ReportType.ERROR)
-private class Success(name: String, message: String): Card(name, message, ReportType.SUCCESS)
-private class Normal(name: String, message: String): Card(name, message, ReportType.NORMAL)
+private class Warning(name: String, message: String) : Card(name, message, ReportType.WARNING)
+private class Error(name: String, message: String) : Card(name, message, ReportType.ERROR)
+private class Success(name: String, message: String) : Card(name, message, ReportType.SUCCESS)
+private class Normal(name: String, message: String) : Card(name, message, ReportType.NORMAL)
 
 /**
  * This class provides the functionality to log messages and print a html report out of them.
@@ -114,7 +123,7 @@ class ReportBuilder(val reportTitle: String = "Run-Report") {
                 title(reportTitle)
                 style {
                     unsafe {
-                        + Path("src/main/kotlin/utils/ReportBuilder/report.css").readText()
+                        +Path("src/main/kotlin/utils/report/report.css").readText()
                     }
                 }
                 script {
@@ -124,7 +133,7 @@ class ReportBuilder(val reportTitle: String = "Run-Report") {
                             const content = document.getElementById(id);
                             content.classList.toggle('show');
                         }
-                    """.trimIndent()
+                        """.trimIndent()
                     }
                 }
             }
@@ -144,13 +153,13 @@ class ReportBuilder(val reportTitle: String = "Run-Report") {
         outputFile.writeText(html)
         val absolutePath = "file://" + outputFile.absolutePathString()
         print("\n\n")
-        println( BOLD + BLUE + "OPEN RUN REPORT (ctrl + lmb): " + absolutePath + RESET)
+        println(BOLD + BLUE + "OPEN RUN REPORT (ctrl + lmb): " + absolutePath + RESET)
         print("\n\n")
     }
 }
 
-private const val RED = "\u001B[31m"
-private const val GREEN = "\u001B[32m"
+// private const val RED = "\u001B[31m"
+// private const val GREEN = "\u001B[32m"
 private const val BLUE = "\u001B[34m"
 private const val BOLD = "\u001B[1m"
 private const val RESET = "\u001B[0m"
