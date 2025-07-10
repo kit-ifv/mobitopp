a3c30bf987d9d8d5b37abc9cf54b822af579e6c8
zq6966 <zq6966@partner.kit.edu>
2025-07-10 17:24:30 +0200

    detekt + Added inline svg.

--- src/test/kotlin/utils/ReportBuilderTest.kt
+++ src/test/kotlin/utils/ReportBuilderTest.kt
@@ -1,35 +1,37 @@
 package utils
 
-import utils.ReportBuilder.ReportBuilder
+import utils.report.ReportBuilder
 import kotlin.io.path.Path
-import kotlin.test.Test
 
-class ReportBuilderTest {
-    @Test
-    fun testReport() {
-        val builder = ReportBuilder()
-        builder.addWarning("Warning", "You overused the number of calls...")
-        builder.addWarning("Too many parsing faults", "It occured, that class XYZ had too many parsing faults during execution of the long-term-module.")
-        builder.addSuccess("Successfully completed the number of calls.",
-            art)
-        builder.addError("Error","oh noooo we failed")
-        builder.addNormalMessage("Nothing happened", "Not even here")
-        builder.printReport(Path("src/test/resources/"))
-    }
+fun main() {
+    val builder = ReportBuilder()
+    builder.addWarning("Warning", "You overused the number of calls...")
+    builder.addWarning(
+        "Too many parsing faults",
+        "It occured, that class XYZ had too many parsing faults during execution of the long-term-module."
+    )
+    builder.addSuccess(
+        "Successfully completed the number of calls.",
+        Art
+    )
+    builder.addError("Error", "oh noooo we failed")
+    builder.addNormalMessage("Nothing happened", "Not even here")
+    builder.printReport(Path("src/test/resources/"))
 }
 
-val art = "⡿⡿⠟⠓⠛⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n" +
-        "⣷⣶⣾⣿⣷⣶⣤⣬⣟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n" +
-        "⣿⣟⣱⡿⠿⠿⠿⣮⣻⣿⣿⣿⣿⣏⢿⣿⡿⢁⢀⣀⣀⣀⣬⣉⣙⠋⠛⠿⢿⣿\n" +
-        "⣟⣛⣡⣤⣤⣁⣀⣄⣉⣻⣿⣿⣿⣿⠛⡿⠻⠛⠭⠿⡿⠯⣭⣟⡻⢿⣶⣦⣀⢙\n" +
-        "⣿⣿⣿⣯⣝⣛⣛⣛⣭⣾⣿⣿⣿⢇⣨⢶⣿⣶⡾⢶⣶⡶⢤⣤⣤⣀⠟⢉⣛⣓\n" +
-        "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣿⠿⠻⠿⢿⣿⣷⣶⣮⣭⣭⣭⣴⣾⡻⣮⣵\n" +
-        "⣿⣿⣿⣿⣿⠟⢋⣽⣿⣿⣿⣿⡿⢿⡿⠿⠿⣆⣉⠻⣿⣿⣿⣿⣿⣿⣿⣷⣽⣿\n" +
-        "⣿⣿⣿⠟⢁⣶⣿⣿⣿⣿⣟⢋⣀⣒⣀⣐⣫⡍⠛⠿⠪⠻⣿⣿⣿⣿⣿⣿⣿⣿\n" +
-        "⣿⣿⢏⠀⣿⣏⣠⣽⣍⣍⣡⣾⣿⣿⣟⣋⣋⣓⣓⣼⣥⣤⡈⠻⣿⣿⣿⡏⣿⣿\n" +
-        "⣿⣿⡌⢦⢻⣮⡁⣼⣿⢭⣭⣉⣭⣭⣭⣉⣭⢛⡉⣛⢛⡛⠛⣠⡌⣿⣿⣧⣿⣿\n" +
-        "⣙⢿⣿⡜⣿⣿⣷⡹⠿⠿⠿⠿⠿⠟⠿⠛⠿⠻⠿⠿⠿⢃⣴⣿⢣⣿⣿⣿⣿⣿\n" +
-        "⣿⣿⣿⣿⡜⣿⣿⡗⠦⣀⣀⢀⠐⠒⠀⠀⠀⢀⡀⠀⣠⣾⣿⢏⣾⢏⢽⣻⣿⣿\n" +
-        "⣿⣿⣿⠈⠙⣾⣏⢧⣼⣿⣟⣛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣼⡋⠿⣾⣿⣿⣾\n" +
-        "⢿⣿⣿⠐⠄⠺⣿⣾⢟⣻⣿⣿⣛⡿⠿⢿⣾⣟⣭⡾⣿⣿⣿⡟⣿⣾⡻⣿⠿⠛\n" +
-        "⠀⡹⣿⡆⠈⣠⣿⣷⣿⣿⣿⣿⣿⣿⣿⣷⣬⡛⣻⣿⣿⣿⣿⣹⢖⠝⠁⡳⣾⣾"
+@Suppress("TopLevelPropertyNaming")
+const val Art = "⡿⡿⠟⠓⠛⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n" +
+    "⣷⣶⣾⣿⣷⣶⣤⣬⣟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n" +
+    "⣿⣟⣱⡿⠿⠿⠿⣮⣻⣿⣿⣿⣿⣏⢿⣿⡿⢁⢀⣀⣀⣀⣬⣉⣙⠋⠛⠿⢿⣿\n" +
+    "⣟⣛⣡⣤⣤⣁⣀⣄⣉⣻⣿⣿⣿⣿⠛⡿⠻⠛⠭⠿⡿⠯⣭⣟⡻⢿⣶⣦⣀⢙\n" +
+    "⣿⣿⣿⣯⣝⣛⣛⣛⣭⣾⣿⣿⣿⢇⣨⢶⣿⣶⡾⢶⣶⡶⢤⣤⣤⣀⠟⢉⣛⣓\n" +
+    "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣿⠿⠻⠿⢿⣿⣷⣶⣮⣭⣭⣭⣴⣾⡻⣮⣵\n" +
+    "⣿⣿⣿⣿⣿⠟⢋⣽⣿⣿⣿⣿⡿⢿⡿⠿⠿⣆⣉⠻⣿⣿⣿⣿⣿⣿⣿⣷⣽⣿\n" +
+    "⣿⣿⣿⠟⢁⣶⣿⣿⣿⣿⣟⢋⣀⣒⣀⣐⣫⡍⠛⠿⠪⠻⣿⣿⣿⣿⣿⣿⣿⣿\n" +
+    "⣿⣿⢏⠀⣿⣏⣠⣽⣍⣍⣡⣾⣿⣿⣟⣋⣋⣓⣓⣼⣥⣤⡈⠻⣿⣿⣿⡏⣿⣿\n" +
+    "⣿⣿⡌⢦⢻⣮⡁⣼⣿⢭⣭⣉⣭⣭⣭⣉⣭⢛⡉⣛⢛⡛⠛⣠⡌⣿⣿⣧⣿⣿\n" +
+    "⣙⢿⣿⡜⣿⣿⣷⡹⠿⠿⠿⠿⠿⠟⠿⠛⠿⠻⠿⠿⠿⢃⣴⣿⢣⣿⣿⣿⣿⣿\n" +
+    "⣿⣿⣿⣿⡜⣿⣿⡗⠦⣀⣀⢀⠐⠒⠀⠀⠀⢀⡀⠀⣠⣾⣿⢏⣾⢏⢽⣻⣿⣿\n" +
+    "⣿⣿⣿⠈⠙⣾⣏⢧⣼⣿⣟⣛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣼⡋⠿⣾⣿⣿⣾\n" +
+    "⢿⣿⣿⠐⠄⠺⣿⣾⢟⣻⣿⣿⣛⡿⠿⢿⣾⣟⣭⡾⣿⣿⣿⡟⣿⣾⡻⣿⠿⠛\n" +
+    "⠀⡹⣿⡆⠈⣠⣿⣷⣿⣿⣿⣿⣿⣿⣿⣷⣬⡛⣻⣿⣿⣿⣿⣹⢖⠝⠁⡳⣾⣾"
