0ec0c227402711541eb383bb2ebe4b98f1067996
zq6966 <ueida@student.kit.edu>
2025-07-30 13:11:25 +0200

    replaced HouseholdId

--- src/main/kotlin/domain/simulation/agent/HouseholdAgent.kt
+++ src/main/kotlin/domain/simulation/agent/HouseholdAgent.kt
@@ -3,16 +3,17 @@ package domain.simulation.agent
 import Mutable
 import core.events.Resource
 import core.events.SubscribableResource
-import domain.synthesis.data.Household
 import domain.synthesis.data.HouseholdId
 import domain.synthesis.data.IHousehold
-import utils.random.SeededActor
+import kotlin.random.Random
 
 @Mutable
 abstract class HouseholdAgent(
     override val id: HouseholdId,
     seed: Long,
-) : SeededActor<Household>(seed), IHousehold, SubscribableResource<PersonAgent> {
+) : IHousehold, SubscribableResource<PersonAgent> {
+
+    final override val random: Random by lazy { Random(id.value + seed) }
 
     abstract override val members: Set<PersonAgent>
     abstract override val cars: Set<PrivateCarAgent>
