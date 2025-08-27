8a8f93b665b14e7daea9568715a9dbdb0d75b570
ar0305 <ar0305@kit.edu>
2025-08-27 12:17:49 +0200

    add missing changes of ScatterPlotterTest.kt

--- src/test/kotlin/core/results/plots/ScatterPlotterTest.kt
+++ src/test/kotlin/core/results/plots/ScatterPlotterTest.kt
@@ -1,48 +1,56 @@
 package core.results.plots
 
-import core.modelsteps.asResource
 import kotlin.random.Random
 import kotlin.test.Test
 
 class ScatterPlotterTest {
 
-    private fun plotterBuilder(plotName: String)
-    = forData { data }
-        .groupBy { it.first}
-        .plot {it.second.second}
-        .over {it.second.first}
-        .colorBy {_, g, _, _ -> g}
-        .withStyle {
-            colorMap = {it -> colors[it]!! }
-            name = plotName
-        }
+    private fun plotterBuilder() = forData { data }
+        .groupBy { it.first }
+        .plot { it.second.second }
+        .over { it.second.first }
 
     @Test
     fun testScatterPlot() {
-        plotterBuilder("scatter").asScatterPlot().plot()
+        plotterBuilder().asScatterPlot {
+            name = "scatter"
+            coloring = { randomColor() }
+        }.plot(testPlotResultDir)
     }
 
     @Test
     fun testComparison() {
-        plotterBuilder("scatter_comparison").compareToResource(comparisonData.asResource("comparison", "I made it up"))
-            .groupBy { it.first}
-            .plot { it.second.second }
-            .over({ it.second.first }, { it }, "")
-            .asScatterPlot()
-            .plot()
+        plotterBuilder().compareTo {
+            forData {
+                comparisonData
+            }.groupBy {
+                it.first
+            }.plot {
+                it.second.second
+            }.over {
+                it.second.first
+            }
+        }.asScatterPlot {
+            name = "scatter_comparison"
+            coloring = { colors[it]!! }
+        }.plot(testPlotResultDir)
     }
 
     val random = Random(0)
-    val data_fun = {x: Number -> x.toDouble() + 30*random.nextDouble()}
-    val data = (0..100).map { listOf(
-        Pair("data1", Pair(it, data_fun(it))),
-        Pair("data2", Pair(it, 20 + data_fun(it/2))),
-        ) }.flatten()
+    val dataFun = { x: Number -> x.toDouble() + 30 * random.nextDouble() }
+    val data = (0..100).map {
+        listOf(
+            Pair("data1", Pair(it, dataFun(it))),
+            Pair("data2", Pair(it, 20 + dataFun(it / 2))),
+        )
+    }.flatten()
 
-    val comparisonData = (0..100).map { listOf(
-        Pair("data1", Pair(it, 100 - data_fun(it))),
-        Pair("data2", Pair(it, 100 - 20 + data_fun(it/2))),
-        ) }.flatten()
+    val comparisonData = (0..100).map {
+        listOf(
+            Pair("data1", Pair(it, 100 - dataFun(it))),
+            Pair("data2", Pair(it, 100 - 20 + dataFun(it / 2))),
+        )
+    }.flatten()
 
     val colors = mapOf("data1" to RGB(0, 255, 0), "data2" to RGB(255, 0, 0))
 }
