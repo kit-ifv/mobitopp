6e56f4d47b278731a2103d59fc720af7db2f0b81
Anton <ueida@student.kit.edu>
2025-09-07 18:02:34 +0200

    added more tests of half float conversions

--- src/test/kotlin/domain/shared/datastructure/matrix/binary/HalfFloatTest.kt
+++ src/test/kotlin/domain/shared/datastructure/matrix/binary/HalfFloatTest.kt
@@ -2,6 +2,7 @@ package domain.shared.datastructure.matrix.binary
 
 import org.junit.jupiter.api.Assertions.*
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class HalfFloatTest {
     @Test
@@ -9,5 +10,8 @@ class HalfFloatTest {
         assertEquals(Double.NaN, Double.NaN.toHalfFloat().fromHalfFloat())
         assertEquals(Double.POSITIVE_INFINITY, 8000000.0.toHalfFloat().fromHalfFloat())
         assertEquals(0.0, 0.0.toHalfFloat().fromHalfFloat())
+        assertEquals(Double.NaN, -0.0001.toHalfFloat().fromHalfFloat())
+        assertEquals(65504.0, 65504.0.toHalfFloat().fromHalfFloat())
+        assertEquals(Double.POSITIVE_INFINITY, 65504.1.toHalfFloat().fromHalfFloat())
     }
 }
