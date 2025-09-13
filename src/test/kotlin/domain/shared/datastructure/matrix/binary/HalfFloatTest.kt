d91753ce6712c7ec289794d9c9a02fc37525e2e1
Anton <ueida@student.kit.edu>
2025-09-13 23:45:34 +0200

    added helper script for printing of halffloat values

--- src/test/kotlin/domain/shared/datastructure/matrix/binary/HalfFloatTest.kt
+++ src/test/kotlin/domain/shared/datastructure/matrix/binary/HalfFloatTest.kt
@@ -1,6 +1,8 @@
 package domain.shared.datastructure.matrix.binary
 
+import org.jetbrains.kotlin.types.ConstantValueKind
 import org.junit.jupiter.api.Assertions.*
+import utils.collections.toBinaryRepresentation
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -13,5 +15,24 @@ class HalfFloatTest {
         assertEquals(Double.NaN, -0.0001.toHalfFloat().fromHalfFloat())
         assertEquals(65504.0, 65504.0.toHalfFloat().fromHalfFloat())
         assertEquals(Double.POSITIVE_INFINITY, 65504.1.toHalfFloat().fromHalfFloat())
+    }
+
+    @Test
+    fun equalityTests() {
+        assertEquals(0.0, 0.0.toHalfFloat().fromHalfFloat())
+        assertEquals(0.001, 0.001.toHalfFloat().fromHalfFloat())
+    }
+}
+
+/**
+ * helper program for printing some transformed values.
+ */
+fun main() {
+    println("| bits | double value | double bits | ")
+    println("| --- | --- | --- |")
+    for(i in (UShort.MIN_VALUE.toInt() until UShort.MAX_VALUE.toInt() + 1).step(4)) {
+        val short = i.toUShort()
+        val transformedResult = short.toShort().fromHalfFloat()
+        println("| ${short.toString(2)}| $transformedResult | ${transformedResult.toBits().toString(2)} |")
     }
 }
