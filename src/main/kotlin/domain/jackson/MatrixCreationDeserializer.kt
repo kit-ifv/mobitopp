ab77e169c0c0194be4bc476841aee44a7a7094fe
zq6966 <zq6966@partner.kit.edu>
2025-10-10 15:16:15 +0200

    moved matrix creation to reengineering. I think it is unlikely, that subprojects implement their own methods. So leaving it to only reengineering to register new ones is sufficient.

---
+++ src/main/kotlin/domain/jackson/MatrixCreationDeserializer.kt
@@ -0,0 +1,44 @@
+package domain.jackson
+
+
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.module.SimpleModule
+import domain.shared.datastructure.matrix.KeyBasedMatrixCreation
+import domain.shared.datastructure.matrix.VisumMatrixCreator
+import domain.shared.datastructure.matrix.ZoneMatrixCreation
+
+class MatrixCreation: SimpleModule("MatrixCreation") {
+    init {
+        addDeserializer(ZoneMatrixCreation::class.java, MatrixDeserializer() )
+    }
+}
+
+/**
+ * All deserializable ZoneMatrixCreation-Methods, that can be used in the Yaml configuration, need to be registered here.
+ */
+class MatrixDeserializer: JsonDeserializer<ZoneMatrixCreation>() {
+    /**
+     * Add new/other ZoneMatrixCreationMethods to this map.
+     */
+    val deserializers: Map<String, ZoneMatrixCreation> = mapOf(
+        "visum" to VisumMatrixCreator,
+        "keyBased" to KeyBasedMatrixCreation
+    )
+
+    override fun deserialize(
+        p: JsonParser,
+        ctxt: DeserializationContext?
+    ): ZoneMatrixCreation? {
+        val node = p.codec.readTree<JsonNode>(p)
+
+        val type = when {
+            node.isTextual -> node.asText()
+            else -> node.get("type").asText()
+        }
+        return deserializers[type] ?: error("Unknown type: $type. Can't deserialize $type. Known types: ${deserializers.keys}")
+
+    }
+}
