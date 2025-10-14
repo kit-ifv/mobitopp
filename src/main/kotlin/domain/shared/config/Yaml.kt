749a64784453f882fc57c135ff9cdde4122192c2
zq6966 <zq6966@partner.kit.edu>
2025-10-14 23:07:08 +0200

    more documentation.

--- src/main/kotlin/domain/shared/config/Yaml.kt
+++ src/main/kotlin/domain/shared/config/Yaml.kt
@@ -10,8 +10,23 @@ import domain.jackson.ZoneMatrixCreation
 import java.nio.file.Path
 
 /**
- * To register new json mappers/parser in a subproject create a directory META-INF/services/
+ * To register new json mappers/parser in a subproject create a directory `META-INF/services/`
  * in src/main/resources/ of the subproject.
+ *
+ * In there add a `package.name.myInterface/Class` so the serviceloader can look for implementations of myClass/
+ * myInterface in that subproject. The file should contain the package-path to the implementation of that class/
+ * interface of that subproject.
+ *
+ * For example, if you want to register a new class SubProjectMapper in a subproject:
+ * ```kotlin
+ * package domain.subproject.yaml
+ * class SubProjectMapper: SimpleModule("MyMapper") { ... }
+ * ```
+ * create a`com.fasterxml.jackson.databind.Module` file in the META-INF.services directory and add the following content
+ * ```
+ * domain.subproject.yaml.SubProjectMapper
+ * ```
+ * This way the ServiceLoader finds it in the `.findAndRegisterModules()` step.
  */
 object Yaml {
     val mapper = ObjectMapper(YAMLFactory())
