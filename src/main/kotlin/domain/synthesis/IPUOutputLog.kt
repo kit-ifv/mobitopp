fb81ba5431e0123f414c132a5e008c035ee96338
fi7528 <fi7528@kit.edu>
2025-10-22 15:29:42 +0200

    safety commit berfore removeing generic

---
+++ src/main/kotlin/domain/synthesis/IPUOutputLog.kt
@@ -0,0 +1,22 @@
+package domain.synthesis
+
+import kotlin.math.abs
+import kotlin.math.max
+
+data class IPUOutputLog(
+    val description: String,
+    val expected: Int,
+    val actual: Int,
+
+    ) {
+    val difference: Int = expected - actual
+    val quotientDifference: Double = run {
+
+        val exp = if (expected == 0) 1e-9 else expected.toDouble()
+        val act = if (actual == 0) 1e-9 else actual.toDouble()
+        max(exp / act, act / exp)
+
+    }
+
+    val percentDifference = abs(difference.toDouble()) / max(1, expected)
+}
