610dcdbf700fea5e8cc5f0eefb5a073f39d89594
Anton <ueida@student.kit.edu>
2025-11-18 22:21:25 +0100

    fixed cursed initialization bug. The serviceloader api tries to initialize a Repo in a subproject, which uses the 'val modeChoiceModelType' though that isn't initialized yet.
    Changed those vals to function calls which seem to fix the issue.

--- src/main/kotlin/domain/jackson/ChoiceModelDeserializers.kt
+++ src/main/kotlin/domain/jackson/ChoiceModelDeserializers.kt
@@ -13,26 +13,40 @@ import domain.simulation.behavior.legacyModeChoiceBuilder
 import edu.kit.ifv.mobitopp.discretechoice.models.FixedChoiceModel
 import edu.kit.ifv.mobitopp.discretechoice.models.UtilityBasedChoiceModel
 
-val destinationChoiceModelType: JavaType = TypeFactory.defaultInstance().constructParametricType(
+/**
+ * Getter for the `DestinationChoiceModelType` aka `UtilityBasedChoiceModel<Location, DestinationChoiceCharacteristics>`.
+ *
+ * *(This is not a val because weird stuff happens when subprojects use that val. Serviceloader tries to access it
+ * before it is initialized. Generally initialization dependencies between top level vals seem to not be well-defined
+ * in kotlin.)*
+ */
+fun getDestinationChoiceModelType(): JavaType = TypeFactory.defaultInstance().constructParametricType(
     UtilityBasedChoiceModel::class.java,
     Location::class.java,
     DestinationChoiceCharacteristics::class.java
 )
 
 val DestinationChoiceModule = GenericKeyValueBuilder(
-    destinationChoiceModelType,
+    getDestinationChoiceModelType(),
     mapOf("legacyDestinationChoiceModel" to legacyDestinationChoiceBuilder.build(DestinationChoiceParameters())),
     loadFromSubmodules = true
 ).getModule()
 
-val modeChoiceModelType: JavaType = TypeFactory.defaultInstance().constructParametricType(
+/**
+ * Getter for the `ModeChoiceModelType` aka `FixedChoiceModel<Mode, ModeChoiceCharacteristics>`.
+ *
+ * (This is not a val because weird stuff happens when subprojects use that val. Serviceloader tries to access it
+ * before it is initialized. Generally initialization dependencies between top level vals seem to not be well-defined
+ * in kotlin.)
+ */
+fun getModeChoiceModelType(): JavaType = TypeFactory.defaultInstance().constructParametricType(
     FixedChoiceModel::class.java,
     Mode::class.java,
     ModeChoiceCharacteristics::class.java
 )
 
 val ModeChoiceModule = GenericKeyValueBuilder(
-    modeChoiceModelType,
+    getModeChoiceModelType(),
     mapOf("legacyModeChoiceModel" to legacyModeChoiceBuilder.build(ModeChoiceParameters())),
     loadFromSubmodules = true
 ).getModule()
