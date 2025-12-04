b95a249f0c42998645514f9a824f604bd99afc8d
Anton <ueida@student.kit.edu>
2025-12-04 17:24:52 +0100

    First fix version based on shallow copy. Introduced a cloneable interface. Required contexts for simulations to be cloneable. Added test for different initialization versions.

---
+++ src/test/kotlin/core/modelsteps/SimulationContextInitTest.kt
@@ -0,0 +1,111 @@
+package core.modelsteps
+
+import edu.kit.ifv.units.CurrencyUnit
+import edu.kit.ifv.units.DistanceUnit
+import org.junit.jupiter.api.Test
+import utils.units.AbsoluteTime
+import java.nio.file.Path
+import kotlin.io.path.Path
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.DurationUnit
+
+data class MyContext(
+    override val execMode: ExecutionMode = ExecutionMode(),
+    override val scenarioName: String = "",
+    override val dataFolder: Path = Path(""),
+    override val simulationSeed: Long = 0,
+    override val resultDir: Path = Path(""),
+    override val simulationStart: AbsoluteTime = AbsoluteTime(0.minutes),
+    override val simulationEnd: AbsoluteTime = AbsoluteTime(0.minutes),
+    override val timeStep: Duration = 0.minutes,
+    override val timeUnit: DurationUnit = DurationUnit.MINUTES,
+    override val costUnit: CurrencyUnit = CurrencyUnit.EUROS,
+    override val distanceUnit: DistanceUnit = DistanceUnit.METERS,
+    var setDuringValidation: Boolean = false
+) : Context, Cloneable<MyContext> {
+    override fun clone(): MyContext {
+        return this.copy()
+    }
+}
+
+/**
+ * Class that does not implement clone correctly.
+ */
+data class MyWrongCloneableContext(
+    override val execMode: ExecutionMode = ExecutionMode(),
+    override val scenarioName: String = "",
+    override val dataFolder: Path = Path(""),
+    override val simulationSeed: Long = 0,
+    override val resultDir: Path = Path(""),
+    override val simulationStart: AbsoluteTime = AbsoluteTime(0.minutes),
+    override val simulationEnd: AbsoluteTime = AbsoluteTime(0.minutes),
+    override val timeStep: Duration = 0.minutes,
+    override val timeUnit: DurationUnit = DurationUnit.MINUTES,
+    override val costUnit: CurrencyUnit = CurrencyUnit.EUROS,
+    override val distanceUnit: DistanceUnit = DistanceUnit.METERS,
+    var setDuringValidation: Boolean = false
+) : Context, Cloneable<MyWrongCloneableContext> {
+    override fun clone(): MyWrongCloneableContext {
+        return this
+    }
+}
+
+class SimulationContextInitTest {
+    @Test
+    fun noSideEffectWhenInitInLambda() {
+        Simulation {
+            MyContext()
+        }.steps {
+            assert(!setDuringValidation) { "If this fails, the variable was set during the " +
+                "validation and not reset for the run. This behaviour should be prevented." }
+            setDuringValidation = true
+        }
+    }
+
+    @Test
+    fun withValFactory() {
+        val myContext = MyContext()
+        Simulation {
+            myContext
+        }.steps {
+            assert(!setDuringValidation) { "If this fails, the variable was set during the " +
+                "validation and not reset for the run. This behaviour should be prevented." }
+            setDuringValidation = true
+        }
+    }
+
+    @Test
+    fun withFactoryMethodFactory() {
+        val myContext: () -> MyContext = { MyContext() }
+        Simulation(
+            myContext
+        ).steps {
+            assert(!setDuringValidation) { "If this fails, the variable was set during the " +
+                "validation and not reset for the run. This behaviour should be prevented." }
+            setDuringValidation = true
+        }
+    }
+
+    /**
+     * This test does not test anything useful. It demonstrates inconsistencies between validation and run when
+     * clone is not a shallow copy.
+     */
+    @Test
+    fun failureWhenCloneIsNotImplementedCorrectly() {
+        val myContext = MyWrongCloneableContext()
+        var num = 0
+        Simulation {
+            myContext
+        }.steps {
+            if (num == 0) assert(!setDuringValidation) // On the first iteration, the validation,
+            // we expect the variable to be false.
+            if (num == 1) assert(setDuringValidation) // On the second iteration, the actual simulation run,
+            // we expect the variable to be true, because
+            // MyWrongCloneableContext does not clone correctly (it does not
+            // clone at all) and therefore the change below
+            setDuringValidation = true
+            num++
+        }
+    }
+}
