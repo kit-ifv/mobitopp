5fed8606f2a9a4b641121f4b24044ef5d79aa682
fi7528 <fi7528@kit.edu>
2026-04-07 16:27:46 +0200

    preparing next reengineering jump

--- src/main/kotlin/domain/synthesis/results/Attractiveness.kt
+++ src/main/kotlin/domain/synthesis/results/Attractiveness.kt
@@ -4,7 +4,10 @@ package domain.synthesis.results
 value class Attractiveness(val value: Double) {
     companion object {
         val DEFAULT = Attractiveness(1.0)
+        val ZERO = Attractiveness(0.0)
     }
+
+    operator fun plus(other: Number): Double = value + other.toDouble()
 }
 
 fun Number.asAttractiveness(): Attractiveness = Attractiveness(this.toDouble())
