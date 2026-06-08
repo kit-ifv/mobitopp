a7df3f8f7f8e903754a067379c706c10d3d1ca4f
ar0305 <jelle.kuebler@kit.edu>
2026-06-08 17:29:24 +0200

    try fix publish to sonatype for subprojects

--- build.gradle.kts
+++ build.gradle.kts
@@ -189,7 +189,7 @@ allprojects {
      */
     apply(plugin = "maven-publish")
     apply(plugin = "signing")
-//    apply(plugin = "io.github.gradle-nexus.publish-plugin:2.0.0")
+    apply(plugin = "io.github.gradle-nexus.publish-plugin:2.0.0")
     project.group = "edu.kit.ifv.mobitopp"
 
 
