3cf7c472d33400f7b04ff8aaf6692c34b9587cbe
Anton <ueida@student.kit.edu>
2026-08-03 14:41:40 +0200

    implement teleportation with distance-matrices

--- src/main/kotlin/edu/kit/ifv/application/steps/parser/LoadImpedance.kt
+++ src/main/kotlin/edu/kit/ifv/application/steps/parser/LoadImpedance.kt
@@ -1,6 +1,7 @@
 package edu.kit.ifv.application.steps.parser
 import edu.kit.ifv.application.steps.HasModes
 import edu.kit.ifv.application.steps.HasMutableImpedance
+import edu.kit.ifv.application.steps.HasZoneRepo
 import edu.kit.ifv.application.steps.MatrixConfig
 import edu.kit.ifv.application.steps.UnitConfig
 import edu.kit.ifv.core.datastructure.matrix.DoubleMatrix
@@ -126,13 +127,14 @@ private fun HasModes.checkConfigKeysAreKnownModes(configText: String, path: Path
  *
  * @receiver The simulation context which can store an impedance model.
  */
-fun HasMutableImpedance.loadTeleportation() = modelStep(
+fun <T> T.loadTeleportation() where T : HasMutableImpedance, T : HasZoneRepo<*, *> = modelStep(
     "create Teleportation impedance for Transport",
 ) {
-    this.impedance = Teleportation()
+    val numLocations = this.zoneRepository.size
+    this.impedance = Teleportation(numLocations)
 }
 
-class Teleportation : Impedance {
+class Teleportation(numLocations: Int) : Impedance {
 
     private val costMetric: CostMetric = CostMetric { _, _ ->
         0.euros
@@ -149,27 +151,14 @@ class Teleportation : Impedance {
 
     override fun durationMetric(mode: Mode, time: Time): DurationMetric = durationMetric
 
-    override fun distanceArray(mode: Mode): DoubleMatrix {
-        TODO("Not yet implemented")
-    }
+    val oneMeterConstDistance = DoubleMatrix(DoubleArray(numLocations * numLocations) { 1.0 }, numLocations)
+    override fun distanceArray(mode: Mode): DoubleMatrix = oneMeterConstDistance
 
-    override fun durationArray(mode: Mode): DoubleMatrix {
-        TODO("Not yet implemented")
-    }
+    val oneSecondConst = DoubleMatrix(DoubleArray(numLocations * numLocations) { 1.0 }, numLocations)
 
-    override fun costArray(mode: Mode): DoubleMatrix {
-        TODO("Not yet implemented")
-    }
+    override fun durationArray(mode: Mode): DoubleMatrix = oneSecondConst
+
+    val zeroEuroConst = DoubleMatrix(DoubleArray(numLocations * numLocations) { 0.0 }, numLocations)
+
+    override fun costArray(mode: Mode): DoubleMatrix = zeroEuroConst
 }
-
-private const val SHOULD_NOT_BE_CALLED = "Should not be called!"
-
-// @Deprecated("dummy impedance should no longer be used, try using Teleportation")
-// val dummyImpedance = object : Impedance {
-//    override fun duration(from: HasZoneId, to: HasZoneId, mode: Mode, time: Time) = 5.minutes
-//    override fun cost(from: HasZoneId, to: HasZoneId, mode: Mode, time: Time) = 5.euros
-//    override fun distance(from: HasZoneId, to: HasZoneId, mode: Mode) = 5.kilometers
-//    override fun costMetric(mode: Mode, time: Time): CostMetric = error(SHOULD_NOT_BE_CALLED)
-//    override fun distanceMetric(mode: Mode): DistanceMetric = error(SHOULD_NOT_BE_CALLED)
-//    override fun durationMetric(mode: Mode, time: Time): DurationMetric = error(SHOULD_NOT_BE_CALLED)
-// }
